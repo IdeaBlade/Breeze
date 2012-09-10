df3b17f8f1508bf610e0e2835bfedddeb1b09632
Jay Traband <jayt@ideablade.com>
2012-09-10 13:45:32 -0700

    Another

--- DocCode/BreezeDocCode/Scripts/tests/entityTests.js
+++ DocCode/BreezeDocCode/Scripts/tests/entityTests.js
@@ -174,7 +174,7 @@ define(["testFns"], function (testFns) {
             // listen for KO to notify that name was refreshed
             ok(value.indexOf("Alfreds") !== -1, 
                 "Knockout notifies UI that CompanyName updated as expected to "+value);
-            start(); // restart the test runner
+            setTimeout(start,0); // restart the test runner
         });
         
         // this refresh query will fill the customer values from remote storage
