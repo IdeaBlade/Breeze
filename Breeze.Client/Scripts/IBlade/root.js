915f3a7fe7c55eb6c3ec21cc4f8a7d203f32291a
Jay Traband <jayt@ideablade.com>
2012-09-19 23:37:20 -0700

    Version change

--- Breeze.Client/Scripts/IBlade/root.js
+++ Breeze.Client/Scripts/IBlade/root.js
@@ -2,7 +2,7 @@
 define(["core", "entityModel"],
 function (core, entityModel) {
     var root = {
-        version: "0.55",
+        version: "0.56",
         core: core,
         entityModel: entityModel
     };
--- Breeze.Client/Scripts/breeze.debug.js
+++ Breeze.Client/Scripts/breeze.debug.js
@@ -10174,7 +10174,7 @@ function (core, m_entityAspect, m_entityMetadata, m_entityManager, m_entityQuery
 define('root',["core", "entityModel"],
 function (core, entityModel) {
     var root = {
-        version: "0.55",
+        version: "0.56",
         core: core,
         entityModel: entityModel
     };
