255ab4c17266959e0af16afc3617a7d0087ec179
EisenbergEffect <rob@bluespire.com>
2012-09-24 17:09:43 -0400

    Adding some pieces for modal dialogs.

---
+++ Samples/Breeze.Inspector/Breeze.Inspector/App/viewmodels/addInspection.js
@@ -0,0 +1,21 @@
+﻿define(function(require) {
+
+    return {
+        selectForm: function(selection) {
+            this.dfd.resolve(selection);
+        },
+        show: function() {
+            var that = this;
+            return jQuery.Deferred(function(dfd) {
+                that.dfd = dfd;
+                require(['text!addInspection.html'], function(html) {
+                    setTimeout(function() {
+                        var view = jQuery(html);
+                        ko.applyBindings(this, view.get(0));
+                        view.modal();
+                    }, 1);
+                });
+            }).promise();
+        }
+    };
+});
--- Samples/Breeze.Inspector/Breeze.Inspector/App/viewmodels/createJob.js
+++ Samples/Breeze.Inspector/Breeze.Inspector/App/viewmodels/createJob.js
@@ -1,16 +1,19 @@
 ﻿define(function(require) {
-    var shell = require('viewmodels/shell');
+    var shell = require('viewmodels/shell'),
+        addInspectionDialog = require('viewmodels/addInspection');
 
     return {
-        states:[],
+        states: [],
         addInspection: function() {
-            
+            addInspectionDialog.show().then(function(selection) {
+                //add to job
+            });
         },
         activate: function() {
             shell.title("[new]");
             shell.subtitle1('Inspector ' + shell.inspector().Name());
             shell.addCommand('save', function() {
-                alert('not implemented');   
+                alert('not implemented');
             });
             shell.addCommand('cancel', function() {
                 alert('not implemented');
