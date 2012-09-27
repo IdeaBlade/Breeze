5b6e9418dc76bbf8b449f60576bf96285e36c703
Jay Traband <jayt@ideablade.com>
2012-09-27 00:16:45 -0700

    version update

--- Breeze.Client/Scripts/IBlade/root.js
+++ Breeze.Client/Scripts/IBlade/root.js
@@ -2,7 +2,7 @@
 define(["core", "entityModel"],
 function (core, entityModel) {
     var root = {
-        version: "0.59",
+        version: "0.60",
         core: core,
         entityModel: entityModel
     };
