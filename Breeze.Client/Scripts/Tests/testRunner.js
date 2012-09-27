b56ca7b2dcdc108569660de9c09f5f8f70a8ed27
Jay Traband <jayt@ideablade.com>
2012-09-26 21:46:12 -0700

    another test

--- Breeze.Client/Scripts/Tests/testRunner.js
+++ Breeze.Client/Scripts/Tests/testRunner.js
@@ -3,19 +3,19 @@
 QUnit.config.autostart = false;
 require.config({ baseUrl: "Scripts/Tests" });
 require([
-      "paramTests",
-      "miscTests",
-       "attachTests",
-      "classRewriteTests",
-      "metadataTests",
-      "entityManagerTests",
-      "entityQueryCtorTests",
-      "entityTests",
-      "queryTests",
-      "queryDatatypeTests",
-      "validateTests",
-      "validateEntityTests",
-       "saveTests"
+      //"paramTests",
+      //"miscTests",
+      "attachTests",
+      //"classRewriteTests",
+      //"metadataTests",
+      //"entityManagerTests",
+      //"entityQueryCtorTests",
+      //"entityTests",
+      //"queryTests",
+      //"queryDatatypeTests",
+      //"validateTests",
+      //"validateEntityTests",
+      // "saveTests"
 
 ], function (testFns) {
     document.getElementById("title").appendChild(document.createElement('pre')).innerHTML = testFns.message;
