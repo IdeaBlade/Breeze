2f47dcf0b3fa7ffc4f12da77534ac7ed408e2145
Ward Bell <wardbell@hotmail.com>
2012-10-08 18:49:37 -0700

    Cleaner KO management of initial view reveal. Uses .hide class and hide vm property

--- Samples/BreezyDevices/BreezyDevices/Scripts/app/peopleViewModel.js
+++ Samples/BreezyDevices/BreezyDevices/Scripts/app/peopleViewModel.js
@@ -3,6 +3,7 @@
     var dataservice = app.dataservice;
 
     var vm = {
+        hide: false,
         people: ko.observableArray([]),
         save: dataservice.saveChanges,
         loadDevices: dataservice.loadDevices,
