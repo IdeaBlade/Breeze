0c4e97bdd178698be7a77142424a81bb987704cd
Ward Bell <wardbell@hotmail.com>
2012-10-15 18:42:18 -0700

    Update with the current breeze package

--- Samples/BreezyDevices/BreezyDevices/App_Start/BreezeConfig.cs
+++ Samples/BreezyDevices/BreezyDevices/App_Start/BreezeConfig.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Web.Http;
 
 [assembly: WebActivator.PreApplicationStartMethod(
@@ -6,34 +6,31 @@ using System.Web.Http;
 [assembly: WebActivator.PostApplicationStartMethod(
     typeof(BreezyDevices.App_Start.BreezeConfig), "RegisterBreezePostStart")]
 
-namespace BreezyDevices.App_Start
-{
-    public static class BreezeConfig
-    {
+namespace BreezyDevices.App_Start {
+  public static class BreezeConfig {
 
-        public static void RegisterBreezePreStart()
-        {
-            GlobalConfiguration.Configuration.Routes.MapHttpRoute(
-                name: "BreezeApi",
-                routeTemplate: "api/{controller}/{action}"
-            );
-        }
-
-        public static void RegisterBreezePostStart()
-        {
-            // Use breeze configuration of Json.Net JsonFormatter instead of the default
-            GlobalConfiguration.Configuration.Formatters.Insert(
-                0, Breeze.WebApi.JsonFormatter.Create());
-
-            // Apply query parameters, expressed as OData URI query strings, 
-            // to results of Web API controller methods that return IQueryable<T>
-            GlobalConfiguration.Configuration.Filters.Add(
-                new Breeze.WebApi.ODataActionFilter());
-
-            // DEVELOPMENT ONLY: initialize the database
-            System.Data.Entity.Database.SetInitializer(
-                new Models.BreezyDevicesDatabaseInitializer());
-
-        }
+    public static void RegisterBreezePreStart() {
+      GlobalConfiguration.Configuration.Routes.MapHttpRoute(
+          name: "BreezeApi",
+          routeTemplate: "api/{controller}/{action}"
+      );
     }
+
+    public static void RegisterBreezePostStart() {
+      // Use breeze configuration of Json.Net JsonFormatter instead of the default
+      GlobalConfiguration.Configuration.Formatters.Insert(
+          0, Breeze.WebApi.JsonFormatter.Create());
+
+      // Apply query parameters, expressed as OData URI query strings, 
+      // to results of Web API controller methods that return IQueryable<T>
+      GlobalConfiguration.Configuration.Filters.Add(
+          new Breeze.WebApi.ODataActionFilter());
+
+      // DEVELOPMENT ONLY: initialize the database
+      //System.Data.Entity.Database.SetInitializer(
+      //    new Models.BreezeSampleDatabaseInitializer());
+      System.Data.Entity.Database.SetInitializer(
+          new Models.BreezyDevicesDatabaseInitializer());
+    }
+  }
 }
