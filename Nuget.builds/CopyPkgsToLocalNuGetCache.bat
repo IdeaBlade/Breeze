be0a74dff444b2b2ef08082a3e8c7d7fd591d26f
Ward Bell <wardbell@hotmail.com>
2012-10-20 23:31:27 -0700

    Add helpful cmd file to copy test packages into your local NuGet cache Remove the Breeze.MVC4WebApiClientSample.nuspec from git

---
+++ Nuget.builds/CopyPkgsToLocalNuGetCache.bat
@@ -0,0 +1,3 @@
+copy Breeze.MVC4WebApi\Breeze.MVC4WebApi.*.nupkg "%LOCALAPPDATA%\NuGet\Cache" /-Y
+copy Breeze.MVC4WebApiClientSample\Breeze.MVC4WebApiClientSample.*.nupkg "%LOCALAPPDATA%\NuGet\Cache" /-Y
+pause
