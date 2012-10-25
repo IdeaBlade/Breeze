bd8fe2daac76ac87166ec872bb2185d03a09f814
Jay Traband <jayt@ideablade.com>
2012-10-25 13:34:32 -0700

    John Papa - one way navigation bug fix

--- Breeze.Client/Scripts/IBlade/root.js
+++ Breeze.Client/Scripts/IBlade/root.js
@@ -2,7 +2,7 @@
 define(["core", "entityModel"],
 function (core, entityModel) {
     var root = {
-        version: "0.64.3",
+        version: "0.64.4",
         core: core,
         entityModel: entityModel
     };
