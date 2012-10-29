3e920885910b37cde605d37d2bc52c7edc9e9549
Ward Bell <wardbell@hotmail.com>
2012-10-29 01:22:13 -0700

    Working backbone sample

---
+++ Samples/CarBones/CarBones/Scripts/app/logger.js
@@ -0,0 +1,35 @@
+﻿app.logger = (function ($) {
+    var logCounter = 1;
+    var logger = {
+        error: error,
+        validationError: validationError,
+        info: info,
+        success: success,
+        warning: warning
+    };
+
+    function error(message) {
+        log("log-error", message);
+    };
+    function validationError(message) {
+        log("log-validation-error", message);
+    };
+    function info(message) {
+        log("log-info", message);
+    };
+    function success(message) {
+        log("log-success", message);
+    };
+    function warning(message) {
+        log("log-warning", message);
+    };
+
+    function log(cssClass, message) {
+        var logmessage = "<div class='" + cssClass + "'>" +
+            logCounter++ + ": " + message + "</div>";
+        $("#logmessages").append(logmessage);
+    }
+
+    return logger;
+
+}(jQuery));
---
+++ Samples/CarBones/CarBones/Scripts/app/viewcontroller.js
@@ -0,0 +1,97 @@
+﻿// ReSharper disable InconsistentNaming
+
+(function ($, Backbone, dataservice) {
+
+    // Get templates
+    var content = $("#content");
+    var carTemplateSource = $("#car-template").html();
+    var optionTemplateSource = $("#option-template").html();
+
+    // Car BB View (extended by stickit)
+    var CarView = Backbone.View.extend({
+        bindings: {
+            '#make-input': 'make',
+            '#model-input': 'model',
+            '#make-desc': 'make',
+            '#model-desc': 'model',
+        },
+        events: {
+            "click #options": "showOptions"
+        },
+        render: function() {
+            this.$el.html(carTemplateSource);
+            this.stickit();
+            return this;
+        },
+        renderOptions: function() {
+            var optionsHost = $("#optionsList", this.$el).empty();
+            var options = this.model.get("options");
+            if (options.length) {
+                options.forEach(
+                    function(option) {
+                        var view = new OptionView({ model: option });
+                        optionsHost.append(view.render().el);
+                    });
+                optionsHost.removeClass("hidden");
+            } else {
+                optionsHost.addClass("hidden");
+            }
+        },
+        // A toggle to hide/show options
+        // will load options from db if not already loaded
+        showOptions: function () {
+            var self = this;
+            var optionsHost = $("#optionsList", self.$el);
+            if (optionsHost.hasClass("hidden")) {
+                dataservice.loadOptionsIfNecessary(self.model)
+                    .then(function () {
+                        self.renderOptions();
+                    });
+            } else {
+                optionsHost.addClass("hidden");
+            }
+        }
+    });
+
+    // Option BB View (extended by stickit)
+    var OptionView = Backbone.View.extend({
+        bindings: {
+            '#name-input': 'name',
+            '#name-desc': 'name'
+        },
+        render: function () {
+            this.$el.html(optionTemplateSource);
+            this.stickit();
+            return this;
+        }
+    });
+
+    var getCars = function() {
+        content.empty();
+        dataservice.getCars()
+            .then(gotCars);
+
+        function gotCars(cars) {
+            cars.forEach(// show cars
+                function(car) {
+                    var view = new CarView({ model: car });
+                    content.append(view.render().el);
+                });
+            enableSave();
+        } 
+    };
+
+    var enableSave = function() {
+        var saveElements = $(".save");
+        saveElements.removeClass("hidden");
+        // only add the click handler once
+        if (enableSave.initialized) { return; }
+        saveElements.click(function() {
+            dataservice.saveChanges();
+        });
+        enableSave.initialized = true;
+    };
+
+    getCars(); 
+    
+})(jQuery, Backbone, app.dataservice);
