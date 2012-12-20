3ca2f2de389b9e911891f15d29735c3a03038b7f
Jay Traband <jayt@ideablade.com>
2012-12-20 14:48:07 -0800

    update nuget ref + BreezeControllerAttribute

--- Breeze.WebApi/BreezeControllerAttribute.cs
+++ Breeze.WebApi/BreezeControllerAttribute.cs
@@ -6,44 +6,40 @@ using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
 
-namespace Breeze.WebApi
-{
-    public class BreezeControllerAttribute : Attribute, IControllerConfiguration
-    {
-        // These instances are stateless and threadsafe so can use static versions for all controller instances
-        private static readonly ODataActionFilterProvider _filterProvider = new ODataActionFilterProvider();
-        private static readonly JsonMediaTypeFormatter _jsonFormatter = JsonFormatter.Create();
+namespace Breeze.WebApi {
 
-        public void Initialize(HttpControllerSettings settings, HttpControllerDescriptor descriptor)
-        {
-            // replace the Web API's QueryActionFilterProvider with Breeze ODataActionFilter
-            settings.Services.Replace(typeof(IFilterProvider), _filterProvider);
+  [AttributeUsage(AttributeTargets.Class)]
+  public class BreezeControllerAttribute : Attribute, IControllerConfiguration {
+    // These instances are stateless and threadsafe so can use static versions for all controller instances
+    private static readonly ODataActionFilterProvider _filterProvider = new ODataActionFilterProvider();
+    private static readonly JsonMediaTypeFormatter _jsonFormatter = JsonFormatter.Create();
 
-            // remove all formatters and add only the Breeze JsonFormatter
-            settings.Formatters.Clear(); 
-            settings.Formatters.Add(_jsonFormatter);
-        }
+    public void Initialize(HttpControllerSettings settings, HttpControllerDescriptor descriptor) {
+      // replace the Web API's QueryActionFilterProvider with Breeze ODataActionFilter
+      settings.Services.Replace(typeof(IFilterProvider), _filterProvider);
 
-        public class ODataActionFilterProvider : IFilterProvider
-        {
-            private readonly IFilter _filter = new ODataActionFilter();
-
-            public IEnumerable<FilterInfo> GetFilters(HttpConfiguration configuration, HttpActionDescriptor actionDescriptor)
-            {
-                return (actionDescriptor == null || !IsIQueryable(actionDescriptor.ReturnType)) ?
-                    Enumerable.Empty<FilterInfo>() :
-                    new[] { new FilterInfo(_filter, FilterScope.Global) };
-            }
-
-            internal static bool IsIQueryable(Type type)
-            {
-                if (type == typeof(IQueryable))
-                    return true;
-                if (type != null && type.IsGenericType)
-                    return type.GetGenericTypeDefinition() == typeof(IQueryable<>);
-                return false;
-            }
-        }
+      // remove all formatters and add only the Breeze JsonFormatter
+      settings.Formatters.Clear();
+      settings.Formatters.Add(_jsonFormatter);
     }
+
+    public class ODataActionFilterProvider : IFilterProvider {
+      private readonly IFilter _filter = new ODataActionFilter();
+
+      public IEnumerable<FilterInfo> GetFilters(HttpConfiguration configuration, HttpActionDescriptor actionDescriptor) {
+        return (actionDescriptor == null || !IsIQueryable(actionDescriptor.ReturnType)) ?
+            Enumerable.Empty<FilterInfo>() :
+            new[] { new FilterInfo(_filter, FilterScope.Global) };
+      }
+
+      internal static bool IsIQueryable(Type type) {
+        if (type == typeof(IQueryable))
+          return true;
+        if (type != null && type.IsGenericType)
+          return type.GetGenericTypeDefinition() == typeof(IQueryable<>);
+        return false;
+      }
+    }
+  }
 
 }
--- _Internal/Sample_WebApi/Controllers/NorthwindIBModelController.cs
+++ _Internal/Sample_WebApi/Controllers/NorthwindIBModelController.cs
@@ -51,6 +51,7 @@ namespace Sample_WebApi.Controllers {
       
   }
 
+  [BreezeController]
   public class NorthwindIBModelController : ApiController {
 
 
