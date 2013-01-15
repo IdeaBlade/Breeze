1f92edb5f05b2f66334e4359358547ef67219cb0
Ward Bell <wardbell@hotmail.com>
2013-01-15 10:24:16 -0800

    Can now restore manager's "original" saveChanges by disabling saveQueuing Tightened up code getting rid of some "this" confusion.

--- Samples/NoDb/NoDb/Scripts/breeze.savequeuing.js
+++ Samples/NoDb/NoDb/Scripts/breeze.savequeuing.js
@@ -1,10 +1,19 @@
 ﻿/**
-Adds "Save Queuing" to new EntityManagers
+Adds "Save Queuing" capability to new EntityManagers
 "Save Queuing" automatically queues and defers an EntityManager.saveChanges call
 when another save is in progress for that manager.
 
 Without "Save Queuing", an EntityManager will throw an exception when
 saveChanges is called while another save is in progress.
+
+Must call EntityManager.enableSaveQueuing(true) to turn it on;
+EntityManager.enableSaveQueuing(false) restores the managers
+base saveChanges method as it was at the time saveQueuing was
+first enabled for it.
+
+This module adds "enableSaveQueuing" to the EntityManager prototype.
+Calling "enableSaveQueuing(true)" adds a new _saveQueuing object 
+to the manager instance.
 
 "Save Queuing" is experimental. It may become part of BreezeJS in future
 although not necessarily in this form or with this API
@@ -20,48 +29,53 @@ touch them at your own risk.
 **/
 (function (breeze, Q) {
 	var EntityManager = breeze.EntityManager;
-	var baseSaveChanges = EntityManager.prototype.saveChanges;
 
     /**
     Enable (default) or disable "Save Queuing" for this manager
     **/
-    EntityManager.prototype.enableSaveQueuing = function(enable) {
-        // Add saveQueuing if not already added to this EntityManager instance
-        var saveQueuing = this._saveQueuing || (this._saveQueuing = new SaveQueuing(this));
-        saveQueuing.isEnabled = enable === undefined ? true : enable;
+	EntityManager.prototype.enableSaveQueuing = function (enable) {
+	    enable = enable === undefined ? true : enable;
+	    if (!this._saveQueuing) {
+	        this._saveQueuing = new SaveQueuing(this);
+	    }
+	    if (enable) {
+	        this.saveChanges = saveChangesWithQueuing;
+	    } else {
+	        this.saveChanges = this._saveQueuing.baseSaveChanges;
+	    }
     };
-    
-    /**
-    Default EntityManager.saveChanges extended with "Save Queuing"
-    **/
-	EntityManager.prototype.saveChanges = function () {
-		// Add saveQueuing if not already added to this EntityManager instance
-		var saveQueuing = this._saveQueuing || (this._saveQueuing = new SaveQueuing(this));
-	    
-		var args = [].slice.call(arguments);
-		if (saveQueuing.isEnabled) {
-			if (saveQueuing.isSaving) {
-			    // save in progress; queue the save for later
-				return saveQueuing.queueSaveChanges(args);
-			} else {
-			    // note that save is in progrees; then save
-				saveQueuing.isSaving = true;
-				return saveQueuing.innerSaveChanges(args);
-			}
-		}
-	    // queuing disabled; just call the base save
-	    return baseSaveChanges.apply(this, args);
-	};
+
 
     var SaveQueuing = function(entityManager) {
-    	this.entityManager = entityManager;
-        this.isEnabled = true;
-        this.isSaving = false;
+        this.entityManager = entityManager;
+        this.baseSaveChanges = entityManager.saveChanges;
+    	this.isSaving = false;
         this.saveQueue = [];
         this.saveSucceeded = defaultSaveSucceeded;
         this.saveFailed = defaultSaveFailed;
         this.QueuedSaveFailedError = QueuedSaveFailedError;
     };
+    
+    SaveQueuing.prototype.isEnabled = function() {
+        return this.entityManager.saveChanges === saveChangesWithQueuing;
+    };
+    
+    /**
+    Replacement for EntityManager.saveChanges, extended with "Save Queuing"
+    **/
+    function saveChangesWithQueuing() {
+        var saveQueuing = this._saveQueuing;
+        var args = [].slice.call(arguments);
+
+        if (saveQueuing.isSaving) {
+            // save in progress; queue the save for later
+            return saveQueuing.queueSaveChanges(args);
+        } else {
+            // note that save is in progrees; then save
+            saveQueuing.isSaving = true;
+            return saveQueuing.innerSaveChanges(args);
+        }
+    }
 
     SaveQueuing.prototype.queueSaveChanges = function (args) {
         var self = this;
@@ -71,14 +85,14 @@ touch them at your own risk.
         var savePromise = deferredSave.promise;
         return savePromise
             .then(function() {return self.innerSaveChanges(args); })
-            .fail(function(error) { self.saveFailed(self, error); });
+            .fail(function(error) { self.saveFailed(error); });
     };
     
     SaveQueuing.prototype.innerSaveChanges = function (args) {
     	var self = this;
-    	return baseSaveChanges.apply(self.entityManager, args)
-            .then(function (saveResult) { self.saveSucceeded(self, saveResult); })
-            .fail(function (error) { self.saveFailed(self, error); });
+    	return self.baseSaveChanges.apply(self.entityManager, args)
+            .then(function (saveResult) { self.saveSucceeded(saveResult); })
+            .fail(function (error) { self.saveFailed(error); });
     };
     
     // Default methods and Error class for initializing new saveQueuing objects
@@ -87,7 +101,8 @@ touch them at your own risk.
     SaveQueuing.QueuedSaveFailedError = QueuedSaveFailedError;
 
     
-    function defaultSaveSucceeded(saveQueuing) {
+    function defaultSaveSucceeded() {
+        var saveQueuing = this;
         var deferredSave = saveQueuing.saveQueue.shift();
         if (deferredSave) {
             deferredSave.resolve();
@@ -97,7 +112,8 @@ touch them at your own risk.
         }
     };
 
-    function defaultSaveFailed(saveQueuing, error) {
+    function defaultSaveFailed(error) {
+        var saveQueuing = this;
     	saveQueuing.isSaving = false;
 		var	saveQueue = saveQueuing.saveQueue;
 		var deferredSave;
