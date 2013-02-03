76437796857b719a242b7d417167ef506fb5e191
wardbell <wardbell@hotmail.com>
2013-02-03 11:43:16 -0800

    Angular sample adapted to use Ng Dependency Injection. Initial commit

---
+++ Samples/Todo-AngularWithDI/Todo-Angular/Scripts/app/controller.js
@@ -0,0 +1,203 @@
+﻿/* Defines the "todo view" controller
+ * Constructor function relies on Ng injector to provide:
+ *     $scope - context variable for the view to which the view binds
+ *     breeze - breeze is a "module" known to the injectory thanks to main.js
+ *     dataservice - injected data and model access component (dataservice.js)
+ *     logger - injected (logger.js)
+ */
+todo.controller('TodoCtrl', function ($scope, breeze, dataservice, logger) {
+
+    var removeItem = breeze.core.arrayRemoveItem;
+    var suspendItemSave;
+
+    $scope.searchText = "";
+    
+    // Beware: this is called a lot!
+    $scope.itemFilter = function (todoItem) {
+        var searchText = $scope.searchText;
+        return searchText ? 
+            // if there is search text, look for it in the description; else return true
+            -1 != todoItem.Description.toLowerCase().indexOf(searchText.toLowerCase()) :
+            true;
+    };
+
+    $scope.newTodo = "";
+    $scope.items = [];
+    $scope.includeArchived = false;
+    
+    $scope.addItem = function() {
+        var item = dataservice.createTodo({
+            Description: $scope.newTodo,
+            CreatedAt: new Date(),
+            IsDone: $scope.allCompleted
+        });
+
+        if (item.entityAspect.validateEntity()) {
+            extendItem(item);
+            $scope.items.push(item);
+            dataservice.saveChanges();
+            $scope.newTodo = "";
+        } else {
+            handleItemErrors(item);
+        }
+    };
+    
+    $scope.edit = function(item) {
+        if (item) {
+            item.isEditing = true;
+        }
+    };
+
+    $scope.completeEdit = function(item) {
+        if (item) {
+            item.isEditing = false;
+            validateAndSaveModifiedItem(item);
+        }
+    };
+    
+    $scope.removeItem = function(item) {
+        removeItem($scope.items, item);
+        item.entityAspect.setDeleted();
+        dataservice.saveChanges();
+    };
+
+    $scope.archiveCompletedItems = function() {
+        var state = getStateOfItems();
+        suspendItemSave = true;
+        state.itemsDone.forEach(function(item) {
+            if (!$scope.includeArchived) {
+                removeItem($scope.items, item);
+            }
+            item.IsArchived = true;
+        });
+        suspendItemSave = false;
+        dataservice.saveChanges();
+    };
+    
+    $scope.purge = function() {
+        return dataservice.purge($scope.getAllTodos);
+    };
+
+    $scope.reset = function() {
+        return dataservice.reset($scope.getAllTodos);
+    };
+
+    $scope.archiveCompletedMessage = function() {
+        var count = getStateOfItems().itemsDoneCount;
+        if (count > 0) {
+            return "Archive " + count + " completed item" + (count > 1 ? "s" : "");
+        }
+        return null;
+    };
+
+    $scope.itemsLeftMessage = function() {
+        var count = getStateOfItems().itemsLeftCount;
+        if (count > 0) {
+            return count + " item" + (count > 1 ? "s" : "") + " left";
+        }
+        return null;
+    };
+
+    $scope.toggleCompleted = function () {
+        $scope.markAllCompleted(!$scope.allCompleted);
+    };
+
+    $scope.markAllCompleted = function(value) {
+        suspendItemSave = true;
+        $scope.items.forEach(function(item) {
+            item.IsDone = value;
+        });
+        suspendItemSave = false;
+        dataservice.saveChanges();
+    };
+
+    $scope.getAllTodos = function () {
+        dataservice.getAllTodos($scope.includeArchived)
+            .then(querySucceeded)
+            .fail(queryFailed);
+    };
+
+    $scope.getAllTodos();
+
+    //#region private functions
+    function querySucceeded(data) {
+        $scope.items = [];
+        data.results.forEach(function (item) {
+            extendItem(item);
+            $scope.items.push(item);
+        });
+        $scope.$apply();
+        
+        logger.info("Fetched Todos " +
+            ($scope.includeArchived ? "including archived" : "excluding archived"));
+    }
+
+    function queryFailed(error) {
+        logger.error(error.message, "Query failed");
+    }
+
+    function extendItem(item) {
+        if (item.isEditing !== undefined) return; // already extended
+
+        item.isEditing = false;
+
+        // listen for changes with Breeze PropertyChanged event
+        item.entityAspect.propertyChanged.subscribe(function() {
+            if (item.isEditing || item.propertyChangedPending || suspendItemSave) {
+                return;
+            }
+            // throttle property changed response to allow time
+            // for other property changes (e.g. "Mark all as complete")
+            item.propertyChangedPending = true;
+            setTimeout(function () { validateAndSaveModifiedItem(item); }, 10);               
+        });
+    }
+    
+    function validateAndSaveModifiedItem(item) {
+        if (item.entityAspect.entityState.isModified()) {
+            if (item.entityAspect.validateEntity()) {
+                dataservice.saveChanges();
+            } else { // errors
+                handleItemErrors(item);
+                item.isEditing = true; // go back to editing
+            }
+        }
+        item.propertyChangedPending = false;
+    }
+    
+    function handleItemErrors(item) {
+        if (!item) { return; }
+        var errs = item.entityAspect.getValidationErrors();
+        if (errs.length == 0) {
+            logger.info("No errors for current item");
+            return;
+        }
+        var firstErr = item.entityAspect.getValidationErrors()[0];
+        logger.error(firstErr.errorMessage);
+        item.entityAspect.rejectChanges(); // harsh for demo 
+    }
+
+    function getStateOfItems() {
+        var itemsDone = [], itemsLeft = [];
+
+        $scope.items.forEach(function (item) {
+            if (item.IsDone) {
+                if (!item.IsArchived) {
+                    itemsDone.push(item); // only unarchived items                
+                }
+            } else {
+                itemsLeft.push(item);
+            }
+        });
+
+        $scope.allCompleted = itemsLeft.length === 0 && $scope.items.length > 0;
+
+        return {
+            itemsDone: itemsDone,
+            itemsDoneCount: itemsDone.length,
+            itemsLeft: itemsLeft,
+            itemsLeftCount: itemsLeft.length
+        };
+    }
+    //#endregion
+});
---
+++ Samples/Todo-AngularWithDI/Todo-Angular/Scripts/app/dataservice.js
@@ -0,0 +1,122 @@
+/* dataservice: data access and model management layer */
+
+// create and add dataservice to the Ng injector
+// constructor function relies on Ng injector to provide breeze & logger services
+todo.factory('dataservice', function (breeze, logger) {
+
+    breeze.config.initializeAdapterInstance("modelLibrary", "backingStore", true);
+    
+    var serviceName = 'api/todos'; // route to the same origin Web Api controller
+   
+    // *** Cross origin service example  ***
+    //var serviceName = 'http://todo.breezejs.com/api/todos'; // controller in different origin
+
+    var manager = new breeze.EntityManager(serviceName);
+    var _isSaving = false;
+    
+    return {
+        getAllTodos: getAllTodos,
+        createTodo: createTodo,
+        saveChanges: saveChanges,
+        purge: purge,
+        reset: reset,
+    };
+
+    /*** implementation details ***/
+ 
+    //#region main application operations
+    function getAllTodos(includeArchived) {
+        var query = breeze.EntityQuery
+                .from("Todos")
+                .orderBy("CreatedAt");
+
+        if (!includeArchived) { // exclude archived Todos
+            // add filter clause limiting results to non-archived Todos
+            query = query.where("IsArchived", "==", false);
+        }
+
+        return manager.executeQuery(query);
+    }
+
+    function createTodo(initialValues) {
+        return manager.createEntity('TodoItem', initialValues);
+    }
+
+    function saveChanges(suppressLogIfNothingToSave) {
+        if (manager.hasChanges()) {
+            if (_isSaving) {
+                setTimeout(saveChanges, 50);
+                return;
+            }
+            _isSaving = true;
+            manager.saveChanges()
+                .then(saveSucceeded)
+                .fail(saveFailed)
+                .fin(saveFinished);
+        } else if (!suppressLogIfNothingToSave) {
+            logger.info("Nothing to save");
+        };
+    }
+    
+    function saveSucceeded(saveResult) {
+        logger.success("# of Todos saved = " + saveResult.entities.length);
+        logger.log(saveResult);
+    }
+    
+    function saveFailed(error) {
+        var reason = error.message;
+        var detail = error.detail;
+        
+        if (reason === "Validation error") {
+            handleSaveValidationError(error);
+            return;
+        }
+        if (detail && detail.ExceptionType &&
+            detail.ExceptionType.indexOf('OptimisticConcurrencyException') !== -1) {
+            // Concurrency error 
+            reason =
+                "Another user, perhaps the server, may have deleted one or all of the todos.";
+            manager.rejectChanges(); // DEMO ONLY: discard all pending changes
+        }
+
+        logger.error(error,
+            "Failed to save changes. " + reason +
+            " You may have to restart the app.");
+    }
+
+    function saveFinished() { _isSaving = false; }
+    
+    function handleSaveValidationError(error) {
+        var message = "Not saved due to validation error";
+        try { // fish out the first error
+            var firstErr = error.entitiesWithErrors[0].entityAspect.getValidationErrors()[0];
+            message += ": " + firstErr.errorMessage;
+        } catch (e) { /* eat it for now */ }
+        logger.error(message);
+    }
+    
+    //#endregion
+    
+    //#region demo operations
+    function purge(callback) {
+        // Todo: breeze should support commands to the controller
+        // Simplified: fails silently
+        $.post(serviceName + '/purge', function () {
+            logger.success("database purged.");
+            manager.clear();
+            if (callback) callback();
+        });
+    }
+
+    function reset(callback) {
+        // Todo: breeze should support commands to the controller
+        // Simplified: fails silently
+        $.post(serviceName + '/reset', function () {
+            logger.success("database reset.");
+            manager.clear();
+            if (callback) callback();
+        });
+    }
+    //#endregion
+
+});
