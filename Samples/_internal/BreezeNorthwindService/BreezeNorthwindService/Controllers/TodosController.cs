285d167e91b25efb7f12f7d6a934230c920879d7
wardbell <wardbell@hotmail.com>
2013-03-01 14:10:01 -0800

    True initial commit git lost my original commit during a failed rebase. Grrr

---
+++ Samples/_internal/BreezeNorthwindService/BreezeNorthwindService/Controllers/TodosController.cs
@@ -0,0 +1,51 @@
+namespace Todo.Controllers {
+    using System;
+    using System.Linq;
+    using System.Web.Http;
+    using Breeze.WebApi;
+    using Models;
+    using Newtonsoft.Json.Linq;
+
+    [BreezeController]
+    public class TodosController : ApiController {
+
+        readonly EFContextProvider<TodosContext> _contextProvider = 
+            new EFContextProvider<TodosContext>();
+
+        // ~/api/todos/Metadata 
+        [HttpGet]
+        public string Metadata() {
+            return _contextProvider.Metadata();
+        }
+
+        // ~/api/todos/Todos
+        // ~/api/todos/Todos?$filter=IsArchived%20eq%20false&$orderby=CreatedAt 
+        [HttpGet]
+        public IQueryable<TodoItem> Todos() {
+            return _contextProvider.Context.Todos;
+        }
+
+        // ~/api/todos/SaveChanges
+        [HttpPost]
+        public SaveResult SaveChanges(JObject saveBundle) {
+            return _contextProvider.SaveChanges(saveBundle);
+        }
+
+        // ~/api/todos/purge
+        [HttpPost]
+        public string Purge()
+        {
+            TodoDatabaseInitializer.PurgeDatabase(_contextProvider.Context);
+            return "purged";
+        }
+
+        // ~/api/todos/reset
+        [HttpPost]
+        public string Reset()
+        {
+            Purge();
+            TodoDatabaseInitializer.SeedDatabase(_contextProvider.Context);
+            return "reset";
+        }
+    }
+}
