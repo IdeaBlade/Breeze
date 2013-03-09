eeb7fa94e85f9b984f78702b758970d439c8dd1b
Jay Traband <jayt@ideablade.com>
2013-03-09 01:36:54 -0800

    Add support for query restrictions to BreezeControllerAttribute

--- Breeze.WebApi/BreezeControllerAttribute.cs
+++ Breeze.WebApi/BreezeControllerAttribute.cs
@@ -29,12 +29,10 @@ namespace Breeze.WebApi {
         // Remove the Web API's "QueryFilterProvider" 
         // and any previously added BreezeQueryableFilterProvider.
         // Add the value from BreezeFilterProvider()
-        // var filterProviders = settings.Services.GetServices(typeof (IFilterProvider));
-        
         settings.Services.RemoveAll(typeof(IFilterProvider),
                                     f => (f.GetType().Name == "QueryFilterProvider")
                                          || (f is BreezeQueryableFilterProvider));
-        settings.Services.Add(typeof(IFilterProvider), GetFilterProvider());
+        settings.Services.Add(typeof(IFilterProvider), GetFilterProvider(_filter));
 
         // remove all formatters and add only the Breeze JsonFormatter
         settings.Formatters.Clear();
@@ -44,14 +42,102 @@ namespace Breeze.WebApi {
     }
 
     /// <summary>
+    /// Gets or sets a value indicating whether query composition should
+    /// alter the original query when necessary to ensure a stable sort order.
+    /// </summary>
+    /// <value>A <c>true</c> value indicates the original query should
+    /// be modified when necessary to guarantee a stable sort order.
+    /// A <c>false</c> value indicates the sort order can be considered
+    /// stable without modifying the query.  Query providers that ensure
+    /// a stable sort order should set this value to <c>false</c>.
+    /// The default value is <c>true</c>.</value>
+    public bool EnsureStableOrdering {
+      get { return _filter.EnsureStableOrdering; }
+      set { _filter.EnsureStableOrdering = value; }
+    }
+
+    /// <summary>
+    /// Gets or sets a value indicating how null propagation should
+    /// be handled during query composition. 
+    /// </summary>
+    /// <value>
+    /// The default is <see cref="HandleNullPropagationOption.Default"/>.
+    /// </value>
+    public HandleNullPropagationOption HandleNullPropagation {
+      get { return _filter.HandleNullPropagation; }
+      set { _filter.HandleNullPropagation = value; }
+    }
+
+    /// <summary>
+    /// Gets or sets the maximum depth of the Any or All elements nested inside the query.
+    /// </summary>
+    /// <remarks>
+    /// This limit helps prevent Denial of Service attacks. The default value is 1.
+    /// </remarks>
+    /// <value>
+    /// The maxiumum depth of the Any or All elements nested inside the query.
+    /// </value>
+    public int MaxAnyAllExpressionDepth {
+      get { return _filter.MaxAnyAllExpressionDepth; }
+      set { _filter.MaxAnyAllExpressionDepth = value; }
+    }
+
+    /// <summary>
+    /// Gets or sets the maximum number of query results to send back to clients.
+    /// </summary>
+    /// <value>
+    /// The maximum number of query results to send back to clients.
+    /// </value>
+    public int PageSize {
+      get { return _filter.PageSize; }
+      set { _filter.PageSize = value; }
+    }
+
+    public AllowedQueryOptions AllowedQueryOptions {
+      get { return _filter.AllowedQueryOptions; }
+      set { _filter.AllowedQueryOptions = value; }
+    }
+
+    public AllowedFunctions AllowedFunctions {
+      get { return _filter.AllowedFunctions; }
+      set { _filter.AllowedFunctions = value; }
+    }
+
+    public AllowedArithmeticOperators AllowedArithmeticOperators {
+      get { return _filter.AllowedArithmeticOperators; }
+      set { _filter.AllowedArithmeticOperators = value; }
+    }
+
+    public AllowedLogicalOperators AllowedLogicalOperators {
+      get { return _filter.AllowedLogicalOperators; }
+      set { _filter.AllowedLogicalOperators = value; }
+    }
+
+    public string AllowedOrderByProperties {
+      get { return _filter.AllowedOrderByProperties; }
+      set { _filter.AllowedOrderByProperties = value; }
+    }
+
+    public int MaxSkip {
+      get { return _filter.MaxSkip; }
+      set { _filter.MaxSkip = value; }
+    }
+
+    public int MaxTop {
+      get { return _filter.MaxTop; }
+      set { _filter.MaxTop = value; }
+    }
+
+
+    /// <summary>
     /// Return the <see cref="IFilterProvider"/> for a Breeze Controller
     /// </summary>
     /// <remarks>
     /// By default returns an <see cref="BreezeQueryableFilterProvider"/>.
     /// Override to substitute a custom provider.
     /// </remarks>
-    protected virtual IFilterProvider GetFilterProvider() {
-      return DefaultFilterProvider;
+    protected virtual IFilterProvider GetFilterProvider(BreezeQueryableAttribute defaultFilter) {
+      return new BreezeQueryableFilterProvider(defaultFilter);
     }
 
     /// <summary>
@@ -66,42 +152,40 @@ namespace Breeze.WebApi {
       return DefaultJsonFormatter;
     }
 
-    /// <summary>
-    /// A <see cref="IFilterProvider"/> that delivers the Breeze
-    /// <see cref="ODataActionFilter"/> to controller action methods 
-    /// that return IQueryable.
-    /// </summary>
-    public class BreezeQueryableFilterProvider : IFilterProvider {
-      private readonly IFilter _filter = new BreezeQueryableAttribute() {AllowedQueryOptions = AllowedQueryOptions.All };
-
-      
-      public IEnumerable<FilterInfo> GetFilters(HttpConfiguration configuration, HttpActionDescriptor actionDescriptor) {
-        if (actionDescriptor == null ||
-          (!IsIQueryable(actionDescriptor.ReturnType)) ||
-          actionDescriptor.GetCustomAttributes<QueryableAttribute>().Any() || // if method already has a QueryableAttribute (or subclass) then skip it.
-          actionDescriptor.GetParameters().Any(parameter => typeof (ODataQueryOptions).IsAssignableFrom(parameter.ParameterType))
-        ) {
-          return Enumerable.Empty<FilterInfo>();
-        }
-
-        return new FilterInfo[] { new FilterInfo(_filter, FilterScope.Global) };
-      }
-
-      internal static bool IsIQueryable(Type type) {
-        if (type == typeof(IQueryable)) return true;
-        if (type != null && type.IsGenericType) {
-          return type.GetGenericTypeDefinition() == typeof(IQueryable<>);
-        }
-        return false;
-      }
-    }
-
+    private BreezeQueryableAttribute _filter = new BreezeQueryableAttribute() { AllowedQueryOptions = AllowedQueryOptions.All };
     private static object __lock = new object();
 
 
     // These instances are stateless and threadsafe so can use static versions for all controller instances
-    private static readonly IFilterProvider DefaultFilterProvider = new BreezeQueryableFilterProvider();
     private static readonly MediaTypeFormatter DefaultJsonFormatter = JsonFormatter.Create();
   }
 
+  internal class BreezeQueryableFilterProvider : IFilterProvider {
+    
+    public BreezeQueryableFilterProvider(BreezeQueryableAttribute filter) {
+      _filter = filter;
+    }
+
+    public IEnumerable<FilterInfo> GetFilters(HttpConfiguration configuration, HttpActionDescriptor actionDescriptor) {
+      if (actionDescriptor == null ||
+        (!IsIQueryable(actionDescriptor.ReturnType)) ||
+        actionDescriptor.GetCustomAttributes<QueryableAttribute>().Any() || // if method already has a QueryableAttribute (or subclass) then skip it.
+        actionDescriptor.GetParameters().Any(parameter => typeof(ODataQueryOptions).IsAssignableFrom(parameter.ParameterType))
+      ) {
+        return Enumerable.Empty<FilterInfo>();
+      }
+
+      return new FilterInfo[] { new FilterInfo(_filter, FilterScope.Global) };
+    }
+
+    internal static bool IsIQueryable(Type type) {
+      if (type == typeof(IQueryable)) return true;
+      if (type != null && type.IsGenericType) {
+        return type.GetGenericTypeDefinition() == typeof(IQueryable<>);
+      }
+      return false;
+    }
+
+    private IFilter _filter;
+  }
 }
