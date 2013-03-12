3722a2490f44a273b5c4afe70eb4cfa1c2aa8b1a
wardbell <wardbell@hotmail.com>
2013-03-12 00:13:58 -0700

    Add validation attributes to Person

--- Samples/_internal/BreezeSampleService/BreezeSampleService/Models/CodeCamperModel.cs
+++ Samples/_internal/BreezeSampleService/BreezeSampleService/Models/CodeCamperModel.cs
@@ -39,7 +39,9 @@ namespace CodeCamper.Models
         }
 
         public int Id { get; set; }
+        [Required, MaxLength(30)]
         public string FirstName { get; set; }
+        [Required, MaxLength(30)]
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Blog { get; set; }
