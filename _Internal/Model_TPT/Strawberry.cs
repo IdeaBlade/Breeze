e47f216b8f67355e89bf3435c0747b07595f9f42
Jay Traband <jayt@ideablade.com>
2013-04-21 00:09:44 -0700

    TPT and TPH Models

---
+++ _Internal/Model_TPT/Strawberry.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+
+namespace ProduceTPT {
+  /// <summary>
+  /// The developer class representing the Strawberry business object class.
+  /// </summary>
+  /// <remarks>
+  /// <para>Place all application-specific business logic in this class.</para>
+  /// <para> 
+  /// The <b>IdeaBlade DevForce Object Mapping Tool</b> generates this class once 
+  /// and will not overwrite changes.
+  /// </para>
+  /// <para>
+  /// This class is the companion to the generated class defining a business object. 
+  /// Properties in the generated class may be overridden in order to change the 
+  /// behavior or add business logic.
+  /// </para>
+  /// </remarks>
+  public partial class Strawberry {
+
+    #region Suggested Customizations
+
+    // Use this factory method template to create new instances of this class
+    //public static Strawberry Create(EntityManager pManager) {
+
+    //  Strawberry aStrawberry = pManager.CreateEntity<Strawberry>();
+
+    //  aStrawberry.Id = Guid.NewGuid();
+    //  String suffix = DateTime.Now.Ticks.ToString();
+    //  aStrawberry.ItemNumber = ("T" + suffix).Substring(0, 8);
+    //  aStrawberry.Name = "teststrawberry";
+    //  aStrawberry.UnitPrice = 1;
+    //  aStrawberry.QuantityPerUnit = "lb";
+    //  aStrawberry.UnitsInStock = 1;
+    //  aStrawberry.UnitsOnOrder = 1;
+    //  aStrawberry.USDACategory = "testcat";
+    //  aStrawberry.Variety = "plump";
+    //  aStrawberry.Description = "red and plump";
+
+    //  aStrawberry.EntityAspect.AddToManager();
+    //  return aStrawberry;
+    //}
+
+  
+
+    #endregion
+
+  }
+
+}
---
+++ _Internal/Model_TPT/Tomato.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+
+namespace ProduceTPT {
+  /// <summary>
+  /// The developer class representing the Tomato business object class.
+  /// </summary>
+  /// <remarks>
+  /// <para>Place all application-specific business logic in this class.</para>
+  /// <para> 
+  /// The <b>IdeaBlade DevForce Object Mapping Tool</b> generates this class once 
+  /// and will not overwrite changes.
+  /// </para>
+  /// <para>
+  /// This class is the companion to the generated class defining a business object. 
+  /// Properties in the generated class may be overridden in order to change the 
+  /// behavior or add business logic.
+  /// </para>
+  /// </remarks>
+  public partial class Tomato {
+
+    // Use this factory method template to create new instances of this class
+    //public static Tomato Create(EntityManager pManager) {
+
+    //  Tomato aTomato = pManager.CreateEntity<Tomato>();
+
+    //  aTomato.Id = Guid.NewGuid();
+    //  String suffix = DateTime.Now.Ticks.ToString();
+    //  aTomato.ItemNumber = ("t" + suffix).Substring(0, 8);
+    //  aTomato.Name = "testtomato";
+    //  aTomato.UnitPrice = 1;
+    //  aTomato.QuantityPerUnit = "lb";
+    //  aTomato.UnitsInStock = 1;
+    //  aTomato.UnitsOnOrder = 1;
+
+    //  aTomato.USDACategory = "testcat";
+    //  aTomato.AboveGround = true;
+    //  aTomato.Variety = "plump";
+    //  aTomato.Description = "red and plump";
+    //  aTomato.Determinate = true;
+
+    //  // aTomato.AddToManager();
+    //  pManager.AddEntity(aTomato);
+    //  return aTomato;
+    //}
+    
+  }
+  
+}
