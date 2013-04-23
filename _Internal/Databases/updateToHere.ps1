ae65f916b778a0610ef93cb425fd8d21b192c494
silviob <silviob@ideablade.com>
2013-04-23 11:39:34 -0700

    updated db powershell scripts and added NorthwindIB.sdf

--- _Internal/Databases/updateToHere.ps1
+++ _Internal/Databases/updateToHere.ps1
@@ -1,14 +1,5 @@
-If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-
-{   
-$arguments = "& '" + $myinvocation.mycommand.definition + "'"
-Start-Process powershell -Verb runAs -ArgumentList $arguments
-Break
-}
-
-
 [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO') | out-null;
-$sqlServerSmo = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Server ("localhost")
+$sqlServerSmo = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Server ("localhost");
 $sourceDir = $sqlServerSmo.Settings.BackupDirectory;
 $sourceDir = $sourceDir.TrimEnd("Backup") + "DATA\";
 
@@ -18,15 +9,30 @@ $destinationDir = $destinationDir.TrimEnd($MyInvocation.MyCommand.Name);
 $file_list = "NorthwindIB", 
              "ProduceTPC",
              "ProduceTPH",
-             "ProduceTPT"
-			 
+             "ProduceTPT";
 
-$service = get-service "MSSQLSERVER" 
-stop-service $service.name -force
+foreach ($file in $file_list) {
+  $datafile = $sourceDir+$file+".mdf";
+  $logfile = $sourceDir+$file+"_log.LDF";
+  
+  $sc = new-object System.Collections.Specialized.StringCollection;
+  $sc.Add($datafile) | Out-Null;
+  $sc.Add($logfile) | Out-Null;
+  
+  if ($sqlServerSmo.databases[$file] -ne $null) {
+	$sqlServerSmo.KillAllProcesses($file);
+    Write-Host "Detaching database "$file;
+	$sqlServerSmo.DetachDatabase($file, $sc);
+  }
+  
+  Write-Host "Copying "$file".mdf and "$file"_log.ldf";
+  Copy-Item $sourceDir$file".mdf" $destinationDir;
+  Copy-Item $sourceDir$file"_log.ldf" $destinationDir;
 
-foreach ($file in $file_list) {  
-  Copy-Item $sourceDir$file".mdf" $destinationDir
-  Copy-Item $sourceDir$file"_log.ldf" $destinationDir
+  Write-Host "Reattaching "$file;
+  Write-Host;
+  $sqlServerSmo.AttachDatabase($file, $sc);
 }
 
-start-service $service.name
+Write-Host "Completed!!!" -BackgroundColor DarkGreen;
+Read-Host "Press [ENTER] to continue...";
