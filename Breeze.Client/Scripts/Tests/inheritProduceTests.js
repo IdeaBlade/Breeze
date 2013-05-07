c403f9d063baa6ea062c090a872d239f3ec8986b
Jay Traband <jayt@ideablade.com>
2013-05-06 19:05:47 -0700

    Guid key comparison bug

--- Breeze.Client/Scripts/Tests/inheritProduceTests.js
+++ Breeze.Client/Scripts/Tests/inheritProduceTests.js
@@ -31,25 +31,20 @@ define(["testFns"], function (testFns) {
         }
     });
 
-    test("fetchEntityByKey ItemsOfProduce", function() {
+    test("EntityKey for ItemsOfProduce", function() {
         var em = newEmX();
-        var blackBeansId = "D234F206-D0C8-40E3-9BF8-0ED190ED0C0C";
+        
         var rdAppleId = "D35E9669-2BAE-4D69-A27A-252B31800B74";
-        var appleType = em.metadataStore.getEntityType("Apple");
-        stop();
-        var item;
-        em.fetchEntityByKey("ItemOfProduce", rdAppleId).then(function(data) {
-            item = data.entity;
-            ok(item, "item should have been found");
-            ok(data.fromCache === false, "should have been from database");
-            ok(item.entityType === appleType);
-            return em.fetchEntityByKey("ItemOfProduce", rdAppleId, true);
-        }).then(function(data2) {
-            item = data2.entity;
-            ok(item, "item should have been found");
-            ok(data2.fromCache === true, "should have been from cache");
-            ok(item.entityType === appleType);
-        }).fail(testFns.handleFail).fin(start)
+        var et = em.metadataStore.getEntityType("ItemOfProduce");
+
+        try {
+            var ek = new EntityKey(et, rdAppleId);
+            ok(false, "shouldn't get here");
+        } catch (e) {
+            ok(e.message.indexOf("EntityKey") >= 0, "message should mention EntityKey");
+        }
+        
+
     });
     
     test("fetchEntityByKey Apple", function () {
