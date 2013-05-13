7b3b2429c0aa5deabe3c7bf461e8b709f540ffa9
Steve Schmitt <steves@ideablade.com>
2013-05-12 22:39:30 -0700

    Moved NHibernate NorthwindContext out of the NorthwindIBModelController

---
+++ _Internal/Sample_WebApi/Controllers/NorthwindContext.cs
@@ -0,0 +1,68 @@
+﻿using Breeze.Nhibernate.WebApi;
+using Models.NorthwindIB.NH;
+
+namespace Sample_WebApi.Controllers
+{
+    public class NorthwindContext : NHContext
+    {
+        public NorthwindContext() : base(NHConfig.OpenSession(), NHConfig.Configuration) { }
+
+        public NorthwindContext Context
+        {
+            get { return this; }
+        }
+        public NhQueryableInclude<Category> Categories
+        {
+            get { return GetQuery<Category>(); }
+        }
+        public NhQueryableInclude<Comment> Comments
+        {
+            get { return GetQuery<Comment>(); }
+        }
+        public NhQueryableInclude<Customer> Customers
+        {
+            get { return GetQuery<Customer>(); }
+        }
+        public NhQueryableInclude<Employee> Employees
+        {
+            get { return GetQuery<Employee>(); }
+        }
+        public NhQueryableInclude<Order> Orders
+        {
+            get { return GetQuery<Order>(); }
+        }
+        public NhQueryableInclude<OrderDetail> OrderDetails
+        {
+            get { return GetQuery<OrderDetail>(); }
+        }
+        public NhQueryableInclude<Product> Products
+        {
+            get { return GetQuery<Product>(); }
+        }
+        public NhQueryableInclude<Region> Regions
+        {
+            get { return GetQuery<Region>(); }
+        }
+        public NhQueryableInclude<Role> Roles
+        {
+            get { return GetQuery<Role>(); }
+        }
+        public NhQueryableInclude<Supplier> Suppliers
+        {
+            get { return GetQuery<Supplier>(); }
+        }
+        public NhQueryableInclude<Territory> Territories
+        {
+            get { return GetQuery<Territory>(); }
+        }
+        public NhQueryableInclude<TimeLimit> TimeLimits
+        {
+            get { return GetQuery<TimeLimit>(); }
+        }
+        public NhQueryableInclude<User> Users
+        {
+            get { return GetQuery<User>(); }
+        }
+
+    }
+}
