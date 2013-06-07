f93fe202f3161c59b0cc05b029579b7befea17fa
Steve Schmitt <steves@ideablade.com>
2013-06-06 23:06:07 -0700

    Added Zza.DataAccess.NH

---
+++ Samples_Unpublished/Zza/Zza.DataAccess.NH/ZzaContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Breeze.Nhibernate.WebApi;
+using Zza.Model;
+
+namespace Zza.DataAccess
+{
+    public class ZzaContext : NHContext
+    {
+        public ZzaContext() : base(NHConfig.OpenSession(), NHConfig.Configuration) { }
+
+        public NhQueryableInclude<Customer> Customers
+        {
+            get { return GetQuery<Customer>(); }
+        }
+        public NhQueryableInclude<Order> Orders
+        {
+            get { return GetQuery<Order>(); }
+        }
+        public NhQueryableInclude<OrderItem> OrderItems
+        {
+            get { return GetQuery<OrderItem>(); }
+        }
+        public NhQueryableInclude<OrderItemOption> OrderItemOptions
+        {
+            get { return GetQuery<OrderItemOption>(); }
+        }
+        public NhQueryableInclude<OrderStatus> OrderStatii
+        {
+            get { return GetQuery<OrderStatus>(); }
+        }
+        public NhQueryableInclude<Product> Products
+        {
+            get { return GetQuery<Product>(); }
+        }
+        public NhQueryableInclude<ProductOption> ProductOptions
+        {
+            get { return GetQuery<ProductOption>(); }
+        }
+        public NhQueryableInclude<ProductSize> ProductSizes
+        {
+            get { return GetQuery<ProductSize>(); }
+        }
+
+    }
+}
