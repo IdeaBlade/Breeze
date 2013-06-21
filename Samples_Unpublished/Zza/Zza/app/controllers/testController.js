9e06ffe489bf98678e11af95f1ee1b67ae6aa11b
Ward Bell <wardbell@hotmail.com>
2013-06-20 23:12:14 -0700

    Example jasmine controller tests in Zza

--- Samples_Unpublished/Zza/Zza/app/controllers/testController.js
+++ Samples_Unpublished/Zza/Zza/app/controllers/testController.js
@@ -2,12 +2,13 @@
     'use strict';
 
     angular.module('app').controller('testCtrl',
-    ['$scope', '$routeParams', 'dataservice','logger',
-    function ($scope, $routeParams, dataservice, logger) {
+    ['$scope', '$routeParams', 'dataservice','logger', testController]);
+    
+    function testController($scope, $routeParams, dataservice, logger) {
         logger.log("testCtrl created") ;
         var orderId = $routeParams.id;
         $scope.orderId = orderId || '<no id>';
         $scope.products = dataservice.products;
-    }]);
+    };
 
 })();
--- Samples_Unpublished/Zza/Zza/test/unit/testControllerSpec.js
+++ Samples_Unpublished/Zza/Zza/test/unit/testControllerSpec.js
@@ -1,10 +1,25 @@
-xdescribe('TestController', function(){
-    'use strict';
+'use strict';
 
-    it('should succeed', function () {
-        console.log('TestController test1');
-        expect(2).toBe(2);
-    });
+describe('testController', function () {
+    var scope, $ctlFactory, logger, ctorArgs;
+
+    beforeEach(module('app'));
+    
+    beforeEach(inject(function($controller, $rootScope) {
+        scope = $rootScope.$new();
+        $ctlFactory = $controller;
+    }));
+
+    beforeEach(function () {
+        logger = sinon.stub({ log: function () { } });
+        ctorArgs = {
+            $scope: scope,
+            $routeParams: {},
+            dataservice: {},
+            logger: logger
+        };
+    }) ;
+
 
     it('breeze should be present', function () {
         expect(breeze).toBeDefined();
@@ -12,6 +27,28 @@ xdescribe('TestController', function(){
 
     it('app module should be present', function () {
         expect(angular.module('app')).toBeDefined();
+    });
+
+    it ('"orderId" should be "<no id>" when no id in routeParams', function() {
+        var ctrl = $ctlFactory("testCtrl", ctorArgs);
+        expect(scope.orderId).toBe('<no id>');
+    });
+    it('"orderId" should be same as id in routeParams', function () {
+        ctorArgs.$routeParams.id ='42';
+        var ctrl = $ctlFactory("testCtrl",ctorArgs);
+        expect(scope.orderId).toEqual('42');
+    });
+    it('should expose products from the dataservice', function () {
+        var products = ['some', 'thing'];
+        ctorArgs.dataservice.products = products;
+        var ctrl = $ctlFactory("testCtrl",ctorArgs);
+        expect(scope.products).toBe(products);
+    });
+
+    it('should report something to the logger', function() {
+        var ctrl = $ctlFactory("testCtrl", ctorArgs);
+        expect(logger.log.calledOnce).toBeTruthy();
+        expect(logger.log.calledWithMatch(/created/i)).toBeTruthy();
     });
 });
 
