e568ec3793702ba61b4037760fbec73b36f1e5b7
Ward Bell <wardbell@hotmail.com>
2013-06-29 20:41:34 -0700

    zza server.js now serves the static files properly and uses standard express modules

--- Samples_Unpublished/Zza/Zza/Zza.ExpressServer/server.js
+++ Samples_Unpublished/Zza/Zza/Zza.ExpressServer/server.js
@@ -1,28 +1,34 @@
 var fs = require("fs");
 var express = require('express');
-var app = express();
 var routes = require('./routes');
 
-app.use(express.bodyParser());
+var app = express();
 
-app.use(app.router);
-app.use(logErrors);
-app.use(errorHandler);
-
-
-app.get('/', function(req,res) {
-    res.sendfile('index.html');
+app.configure(function(){
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.static(__dirname + "/../"));
+    app.use(express.bodyParser());
+   // app.use(express.methodOverride());  // for full REST ... if we need it
+    app.use(app.router);
+    app.use(express.errorHandler());
+    //app.use(logErrors);    // See below
+    //app.use(errorHandler); // See below
 });
-app.get('/breeze/zza/Metadata', routes.getMetadata);
-app.get('/breeze/zza/Products', routes.getProducts);
-app.post('/breeze/zza/SaveChanges', routes.saveChanges);
 
-app.get('/breeze/zza/:slug', routes.get);
+/* data routes */
+app.configure(function() {
+    app.get('/breeze/zza/Metadata', routes.getMetadata);
+    app.get('/breeze/zza/Products', routes.getProducts);
+    app.post('/breeze/zza/SaveChanges', routes.saveChanges);
+    app.get('/breeze/zza/:slug', routes.get);
+});
 
 app.listen(3000);
 console.log('Listening on port 3000');
 
-
+/* Our errorHandler if we don't like the express handler */
+/*
 function errorHandler(err, req, res, next) {
     var status = err.statusCode || 500;
     if (err.message) {
@@ -36,3 +42,4 @@ function logErrors(err, req, res, next) {
     console.error(err.stack);
     next(err);
 }
+ */
