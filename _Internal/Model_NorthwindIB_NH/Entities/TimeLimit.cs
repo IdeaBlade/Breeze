e87d0c83faf6b82bf58c906192f872379b48e061
Steve Schmitt <steves@ideablade.com>
2013-07-08 13:41:14 -0700

    NH: fixed TimeLimit relationship in test model

--- _Internal/Model_NorthwindIB_NH/Entities/TimeLimit.cs
+++ _Internal/Model_NorthwindIB_NH/Entities/TimeLimit.cs
@@ -9,5 +9,8 @@ namespace Models.NorthwindIB.NH
         public virtual System.TimeSpan MaxTime { get; set; }
         public virtual Nullable<System.TimeSpan> MinTime { get; set; }
         public virtual Nullable<int> TimeGroupId { get; set; }
+
+        public virtual TimeGroup TimeGroup { get; set; }
+
     }
 }
