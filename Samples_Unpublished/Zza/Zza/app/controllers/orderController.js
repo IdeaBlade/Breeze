a474aaa0ee4a5f7fb244f7797a238c359c14f25a
Ward Bell <wardbell@hotmail.com>
2013-07-22 10:04:39 -0700

    Zza: refactoring for joint SQL/Mongo version (progress)

--- Samples_Unpublished/Zza/Zza/app/controllers/orderController.js
+++ Samples_Unpublished/Zza/Zza/app/controllers/orderController.js
@@ -6,26 +6,41 @@
         ['$scope', '$routeParams', '$route', 'routes', 'dataservice', controller]);
     
     function controller($scope, $routeParams, $route, routes, dataservice) {
- 
-        // tag comes from nav url; get the current route
-        var tag = $routeParams.tag;
         var hasId = !!$routeParams.id;
-        var route = setTaggedRoute(tag, hasId) || setTaggedRoute();
-        if (!hasId) setProducts();
+        var route = getTaggedRoute();
+        $scope.activeTag = route.tag;
+        setProducts();
         $scope.view = route.templateUrl; // view is used in data-ng-include
         $scope.links = routes.visibleOrderRoutes;
         $scope.cartOrder = dataservice.cartOrder;
         $scope.draftOrder = dataservice.draftOrder;
 
-        function setTaggedRoute(t, hasId) {
-            tag = (t || 'pizza').toLowerCase();
-            $scope.activeTag = tag;
+        // get the current route from the nav args
+        function getTaggedRoute() {            
+            var i, len, pizza, item, result;
+            var tag = (($routeParams.tag) || '').toLowerCase();
             var source = hasId ? routes.detailRoutes : routes.orderRoutes;
-            return source.filter(
-                function (item) { return item.tag === tag })[0];
+            for (i = 0, len = source.length; i < len; i++) {
+                item = source[i];
+                if (item.tag === tag) {
+                    result = item;
+                    break;
+                } else if (item.tag === 'pizza') {
+                    pizza = item; // default route
+                }
+            }
+            if (!result) {
+                if (!pizza) {
+                    throw new Error("No matching route and can't default to 'pizza'");
+                }
+                result = pizza;// fall back to default
+            }
+            return result;           
         }
 
         function setProducts() {
+            if (hasId) return; // products not needed.
+            var tag = $scope.activeTag;
             var products = dataservice.products.filter(
                 function (product) { return product.type == tag; });
             $scope.products = products;
