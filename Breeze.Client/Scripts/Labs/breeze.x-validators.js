df049ba3cd639982125270da007b20531bed34d8
Ward Bell <wardbell@hotmail.com>
2013-08-13 01:17:41 -0700

    breeze.x-validators.js from labs incorporated into breeze.base (a25_) They pass smoke test in CCJS but official tests yet to come

--- Breeze.Client/Scripts/Labs/breeze.x-validators.js
+++ Breeze.Client/Scripts/Labs/breeze.x-validators.js
@@ -6,6 +6,18 @@
     var Validator = breeze.Validator;
     var ctor = Validator; // make it easier to import into breeze later
 
+    // TODO: relocate default messages when incorporating in breeze.Validator
+    Validator.messageTemplates.creditCard =
+        "The %displayName% is not a valid credit card number";
+    Validator.messageTemplates.emailAddress =
+        "The %displayName% '%value%' is not a valid email address";
+    Validator.messageTemplates.phone =
+        "The %displayName% '%value%' is not a valid phone number";
+    Validator.messageTemplates.regularExpression =
+        "The %displayName% '%value%' does not match '%expression%'";
+    Validator.messageTemplates.url =
+        "The %displayName% '%value%' is not a valid url"
+
     /**
     Returns a credit card number validator
     Performs a luhn algorithm checksum test for plausability
@@ -18,18 +30,17 @@
         creditCardProperty.validators.push(Validator.creditCard());
     @method creditCard
     @static
-    @param context {Object} 
     @return {Validator} A new Validator
     **/
-    ctor.creditCard = function(context) {
-        function valFn(v, ctx) {
+    ctor.creditCard = function() {
+        function valFn(v) {
             if (v == null || v === '') return true;
             if (typeof (v) !== 'string') return false;
             v = v.replace(/(\-|\s)/g, ""); // remove dashes and spaces
             if (!v || /\D/.test(v)) return false; // all digits, not empty
             return luhn(v);
         };
-        return new ctor('creditCard', valFn, context);
+        return new ctor('creditCard', valFn);
     };
 
     // http://rosettacode.org/wiki/Luhn_test_of_credit_card_numbers#JavaScript
@@ -38,10 +49,6 @@
             c = +a[b], d += ++e % 2 ? 2 * c % 10 + (c > 4) : c;
         return !(d % 10);
     };
-  
-    // TODO: relocate default message set when incorporating in breeze.Validator
-    Validator.messageTemplates.creditCard =
-        "The %displayName% is not a valid credit card number.";
     
     /**
     Returns a regular expression validator; the expression must be specified
@@ -74,10 +81,6 @@
         return new ctor('regularExpression', valFn, context);
     };
 
-    // TODO: relocate default message set when incorporating in breeze.Validator
-    Validator.messageTemplates.regularExpression =
-        "The %displayName% '%value%' does not match '%expression%'.";
-
     /**
     Returns the email address validator
     @example
@@ -85,22 +88,16 @@
         var personType = em.metadataStore.getEntityType("Person");
         var emailProperty = personType.getProperty("email");
         // Validates that the value of the Person.email property is an email address.
-        emailProperty.validators.push(Validator.email());
+        emailProperty.validators.push(Validator.emailAddress());
     @method emailAddress
     @static
-    @param context {Object} 
     @return {Validator} A new Validator
     **/
-    ctor.emailAddress = function(context) {
-        return makeRegExpValidator(
-            'emailAddress',
-            reEmailAddress,
-            "The %displayName% '%value%' is not a valid email address.");
+    ctor.emailAddress = function() {
+        // See https://github.com/srkirkland/DataAnnotationsExtensions/blob/master/DataAnnotationsExtensions/EmailAttribute.cs
+        var reEmailAddress = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/;
+        return makeRegExpValidator('emailAddress',reEmailAddress);
     };
-
-    // Got the email RegExp from here:
-    // https://github.com/srkirkland/DataAnnotationsExtensions/blob/master/DataAnnotationsExtensions/EmailAttribute.cs
-    var reEmailAddress = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/;
 
     /**
     Returns the phone validator
@@ -122,19 +119,13 @@
         phoneProperty.validators.push(Validator.phone());
     @method phone
     @static
-    @param context {Object} 
     @return {Validator} A new Validator
     **/
-    ctor.phone = function(context) {
-        return makeRegExpValidator(
-            'phone',
-            rePhone,
-            "The %displayName% '%value%' is not a valid phone number.");
+    ctor.phone = function() {
+        // See https://github.com/srkirkland/DataAnnotationsExtensions/blob/master/DataAnnotationsExtensions/Expressions.cs
+        var rePhone = /^((\+|(0(\d+)?[-/.\s]?))[1-9]\d{0,2}[-/.\s]?)?((\(\d{1,6}\)|\d{1,6})[-/.\s]?)?(\d+[-/.\s]?)+\d+$/;
+        return makeRegExpValidator('phone',rePhone);
     };
- 
-    // Got the email RegExp from here:
-    // https://github.com/srkirkland/DataAnnotationsExtensions/blob/master/DataAnnotationsExtensions/Expressions.cs
-    var rePhone = /^((\+|(0(\d+)?[-/.\s]?))[1-9]\d{0,2}[-/.\s]?)?((\(\d{1,6}\)|\d{1,6})[-/.\s]?)?(\d+[-/.\s]?)+\d+$/;
 
     /**
     Returns the URL (protocol required) validator
@@ -146,19 +137,13 @@
         websiteProperty.validators.push(Validator.url());
     @method url
     @static
-    @param context {Object} 
     @return {Validator} A new Validator
     **/
-    ctor.url = function(context) {
-        return makeRegExpValidator(
-         'url',
-          reUrlProtocolRequired,  
-        "The %displayName% '%value%' is not a valid url.");
+    ctor.url = function() {
+        //See https://github.com/srkirkland/DataAnnotationsExtensions/blob/master/DataAnnotationsExtensions/UrlAttribute.cs
+        var reUrlProtocolRequired = /^(https?|ftp):\/\/(((([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-fA-F]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|([a-zA-Z][\-a-zA-Z0-9]*)|((([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-fA-F]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-fA-F]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-fA-F]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-fA-F]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/;
+        return makeRegExpValidator('url', reUrlProtocolRequired);
     };    
-
-    // Got the url-protocol-required RegExp from here:
-    //https://github.com/srkirkland/DataAnnotationsExtensions/blob/master/DataAnnotationsExtensions/UrlAttribute.cs
-    var reUrlProtocolRequired = /^(https?|ftp):\/\/(((([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-fA-F]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|([a-zA-Z][\-a-zA-Z0-9]*)|((([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-fA-F]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-fA-F]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-fA-F]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-fA-F]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/;
 
     /* register these validators */
     Validator.registerFactory(ctor.regularExpression, 'regularExpression');
@@ -198,18 +183,18 @@
     **/
     ctor.makeRegExpValidator = makeRegExpValidator;
     
-    function makeRegExpValidator(validatorName, expression, defaultMessage, context) {
+    function makeRegExpValidator(validatorName, expression, defaultMessage) {
         if (defaultMessage) {
             ctor.messageTemplates[validatorName] = defaultMessage;
         }
         var re = (typeof (expression) === 'string') ? new RegExp(expression) : expression;
-        var valFn = function(v, ctx) {
+        var valFn = function(v) {
             // do not invalidate if empty; use a separate required test
             if (v == null || v === '') return true;
             if (typeof(v) !== 'string') return false;
             return re.test(v);
         };
-        return new ctor(validatorName, valFn, context);
+        return new ctor(validatorName, valFn);
     };
     
 })();
