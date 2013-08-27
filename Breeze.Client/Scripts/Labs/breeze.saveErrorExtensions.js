595fa05c31735bf5309a5805897e012c2cd3aa3a
Ward Bell <wardbell@hotmail.com>
2013-08-26 18:22:45 -0700

    breeze.saveErrorExtension is now an extensible service

--- Breeze.Client/Scripts/Labs/breeze.saveErrorExtensions.js
+++ Breeze.Client/Scripts/Labs/breeze.saveErrorExtensions.js
@@ -1,8 +1,15 @@
 ﻿/*
- * Extend breeze with getSaveErrorMessage,   
- * a utility method that can extract validation error messages and 
+ * Extend breeze with saveErrorMessageService,   
+ * a utility service for processing errors returned from a save.
+ *
+ * `getErrorMessage is its primary method (see example).
+ *
+ * The default implementation extracts validation error messages and 
  * arranges for server validation errors to be removed from an entity 
  * the next time this entity changes in any way.
+ *
+ * The service API includes implementation methods that can be overriden (replaced)
+ * to change the behavior just described
  * 
  * This extension is not prescriptive! 
  * It is only one approach to save error message handling.
@@ -13,6 +20,8 @@
  * http://opensource.org/licenses/mit-license.php
  * Author: Ward Bell
  *
+ * v.1.0
+ *
  * Install:
  *   1) include this script after breeze script
  *   2) use it in your save failure handler
@@ -21,57 +30,86 @@
  *   return manager.saveChanges().then(saveSucceeded, saveFailed);
  *   
  *   function saveFailed(error) {
- *       var msg = 'Save failed: ' + breeze.getSaveErrorMessage(error);
+ *       var msg = 'Save failed: ' + breeze.saveErrorMessageService.getErrorMessage(error);
  *       error.message = msg;
  *       logError(msg, error);
  *       throw error;
  *   }
+ * 
+
 */
 (function() {
 	'use strict';
 
-	breeze.getSaveErrorMessage = getSaveErrorMessage;
+    var service = {
+    	// The main service function
+    	getErrorMessage: getErrorMessage,
 
-	function getSaveErrorMessage(error) {
+    	// Replaceable implementation methods
+    	getEntityName: getEntityName,
+    	getMessageFromEntityError: getMessageFromEntityError,
+    	reviewServerErrors: reviewServerErrors
+    }
+
+	breeze.saveErrorMessageService = service;
+
+    // The main service function
+	function getErrorMessage(error) {
 		var msg = error.message;
 		var entityErrors = error.entityErrors;
 		if (entityErrors && entityErrors.length) {
+			service.reviewServerErrors(entityErrors);
 			return getValidationMessages(entityErrors);
 		}
 		return msg;
 	}
 
+
 	function getValidationMessages(entityErrors) {
-		var isServerError = entityErrors[0].isServerError;
-
+        var isServerError = entityErrors[0].isServerError; // if the first is, they all are
 		try {
-		    return entityErrors.map(function(entityError) {
-		        var name = '';
-		        var entity = entityError.entity;
-		        if (entity) {
-		            clearServerErrorsOnNextChange(isServerError, entity);
-		            name = getErrantEntityName(entity);
-		        }
-
-				return name + '\'' + entityError.errorMessage + '\'';
-			}).join('; <br/>');
+		    return entityErrors.map(service.getMessageFromEntityError).join('; <br/>');
 		} catch (e) {
 			/* eat it for now */
 			return (isServerError ? 'server' : 'client') + ' validation error';
 		}
 	}
 
-	function getErrantEntityName(entity) {
+	// default implementation of service.getMessageFromEntityError
+	function getMessageFromEntityError(entityError){
+        var entity = entityError.entity;
+        if (entity) {
+            var name = service.getEntityName(entity);
+        }
+        name = name ? name += ' - ' : '';
+
+		return name + '\'' + entityError.errorMessage + '\'';
+	}
+
+	// default implementation of service.getEntityName
+	function getEntityName(entity) {
 		var key = entity.entityAspect.getKey();
 		var name = key.entityType.shortName;
 		var id = key.values.join(',');
-		return name + ' (' + id + ') - ';
+		return name + ' (' + id + ')';
 	}
 
-	function clearServerErrorsOnNextChange(isServerError, badEntity) {
 
-		if (!isServerError || 
-			badEntity.entityAspect.entityState.isDetached()) { return; }
+	// default implementation of service.reviewServerErrors
+	function reviewServerErrors(entityErrors){
+		var entitiesWithServerErrors = [];
+		entityErrors.forEach(function(entityError){
+			var entity = entityError.isServerError && entityError.entity;
+			if (entity && entitiesWithServerErrors.indexOf(entity) === -1) {
+				entitiesWithServerErrors.push(entity);
+				clearServerErrorsOnNextChange(entity);
+			}
+		})
+	}
+
+	function clearServerErrorsOnNextChange(badEntity) {
+
+		if (badEntity.entityAspect.entityState.isDetached()) { return; }
 
 		// implemented as a one-time, propertyChanged eventhandler that
 	    // clears the server validation errors if anything happens to this entity
