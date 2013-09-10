6c7db68cee41df1bfff94afe9706825807cf9575
Patrick Walters <patrickwaltersc21@yahoo.com>
2013-09-10 08:52:08 -0500

    Removed extra comma at line 12250
    Wasn't compatible in older browsers

--- Breeze.Client/Scripts/breeze.debug.js
+++ Breeze.Client/Scripts/breeze.debug.js
@@ -12241,7 +12241,7 @@ var EntityManager = (function () {
                     errorName: ve.validator.name,
                     errorMessage: ve.errorMessage,
                     propertyName: ve.propertyName,
-                    isServerError: ve.isServerError,
+                    isServerError: ve.isServerError
                 });
             });
         });
