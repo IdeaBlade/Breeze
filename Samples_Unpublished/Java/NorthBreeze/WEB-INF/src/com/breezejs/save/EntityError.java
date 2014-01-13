a7e1e75cd01d29efa61bbddf00cdf3d09809a123
Steve Schmitt <steves@ideablade.com>
2014-01-12 21:40:05 -0800

    JavaJava: NorthBreeze: Added RelationshipFixer and HibernateContext

--- Samples_Unpublished/Java/NorthBreeze/WEB-INF/src/com/breezejs/save/EntityError.java
+++ Samples_Unpublished/Java/NorthBreeze/WEB-INF/src/com/breezejs/save/EntityError.java
@@ -1,5 +1,19 @@
 package com.breezejs.save;
 
 public class EntityError {
+    public String errorName;
+    public String entityTypeName;
+    public Object[] keyValues;
+    public String propertyName;
+    public String errorMessage;
+	public EntityError(String errorName, String entityTypeName,
+			Object[] keyValues, String propertyName, String errorMessage) {
+		super();
+		this.errorName = errorName;
+		this.entityTypeName = entityTypeName;
+		this.keyValues = keyValues;
+		this.propertyName = propertyName;
+		this.errorMessage = errorMessage;
+	}
 
 }
