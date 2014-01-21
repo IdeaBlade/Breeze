2b122f847c2e824ea05bddce03163830acb6ea89
Steve Schmitt <steves@ideablade.com>
2014-01-21 15:15:49 -0800

    Java: renamed web folders to match NH sample app

--- Samples_Unpublished/Java/NorthBreeze/app/app.js
+++ Samples_Unpublished/Java/NorthBreeze/app/app.js
@@ -5,9 +5,9 @@ var app = angular.module('NorthBreeze', ['ngGrid']);
 
 // Define route objects, which are used by the routeProvider (for loading ng-view) and by the RouteCtrl (for displaying navigation bar)
 app.routes = [
-    { path: '/', name: 'Home', templateUrl: 'app/views/home.html', controller: 'HomeCtrl' },
-    { path: '/customers', name: 'Customers', templateUrl: 'app/views/customers.html', controller: 'CustomerCtrl' },
-    { path: '/orders', name: 'Orders', templateUrl: 'app/views/orders.html', controller: 'OrderCtrl' }
+    { path: '/', name: 'Home', templateUrl: 'App/views/home.html', controller: 'HomeCtrl' },
+    { path: '/customers', name: 'Customers', templateUrl: 'App/views/customers.html', controller: 'CustomerCtrl' },
+    { path: '/orders', name: 'Orders', templateUrl: 'App/views/orders.html', controller: 'OrderCtrl' }
 ];
 
 // Configure the routeProvider, which displays a view in the ng-view div in index.html, based on the URI path (e.g. /customers)
