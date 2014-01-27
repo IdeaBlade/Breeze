6f41324246b1a24403692aeb6e15694eb554217f
Jay Traband <jayt@ideablade.com>
2014-01-27 00:54:36 -0800

    Breeze.net navset work

--- Breeze.NetClient/INavigationSet.cs
+++ Breeze.NetClient/INavigationSet.cs
@@ -18,26 +18,21 @@ namespace Breeze.NetClient {
     void Clear();
   }
 
-  public class NavigationSet<T> : HashSet<T>, INavigationSet where T:IEntity {
+  public class NavigationSet<T> : ICollection<T>, INavigationSet where T:IEntity {
 
     public NavigationSet() {
-
+      _hashSet = new HashSet<T>();
     }
+    private HashSet<T> _hashSet;
 
     public NavigationSet(IEntity parentEntity, NavigationProperty navigationProperty) {
       ((INavigationSet) this).ParentEntity = parentEntity;
     }
 
-    void INavigationSet.Add(IEntity entity) {
-      Add((T)entity);
-    }
-
-    void INavigationSet.Remove(IEntity entity) {
-      Remove((T)entity);
-    }
+    #region Public props
 
     public IEntity ParentEntity {
-      get; 
+      get;
       set;
     }
 
@@ -46,56 +41,103 @@ namespace Breeze.NetClient {
       set;
     }
 
-    internal bool InProcess { get; set; }
+    #endregion
 
+    #region INavigationSet imp
+
+    void INavigationSet.Add(IEntity entity) {
+      AddCore(entity);
+    }
+
+
+    void INavigationSet.Remove(IEntity entity) {
+      Remove((T)entity);
+    }
+
+    #endregion
+
+    #region ICollection<T> implementation
+
+    public void Add(T item) {
+      AddCore(item);
+    }
+
+
+    public void Clear() {
+      _hashSet.Clear();
+    }
+
+    public bool Contains(T item) {
+      return _hashSet.Contains(item);
+    }
+
+    public void CopyTo(T[] array, int arrayIndex) {
+      _hashSet.CopyTo(array, arrayIndex);
+    }
+
+    public int Count {
+      get { return _hashSet.Count; }
+    }
+
+    public bool IsReadOnly {
+      get { throw new NotImplementedException(); }
+    }
+
+    public bool Remove(T item) {
+      return _hashSet.Remove(item);
+    }
+
+    public IEnumerator<T> GetEnumerator() {
+      return _hashSet.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() {
+      return _hashSet.GetEnumerator();
+    }
+
+    #endregion
+
+    #region Internal and Private
+
+    
     private void AddCore(IEntity entity) {
-      // Notes: if being loaded from query it will already be attached by the time it gets here.
-      if (InProcess) return;
-      var parentAspect = ParentEntity.EntityAspect;
-      var entityManager = parentAspect.EntityManager;
-      if (parentAspect.IsDetached || !entityManager.IsLoadingEntity) {
-        
-        Add((T)entity);
-      } else {
-        // relationArray.inProgress may make sense here
-        if (entity.EntityAspect.IsDetached) {
-          using (new BooleanUsingBlock((b) => this.InProcess = b)) {
+      if (_inProcess || _hashSet.Contains( (T) entity)) return;
+      using (new BooleanUsingBlock(b => _inProcess = b)) {
+        var parentAspect = ParentEntity.EntityAspect;
+        var entityManager = parentAspect.EntityManager;
+        if (entityManager.IsLoadingEntity || parentAspect.IsDetached) {
+          _hashSet.Add((T)entity);
+        } else {
+          if (entity.EntityAspect.IsDetached) {
             entity.EntityAspect.Attach(EntityState.Added, entityManager);
           }
+          _hashSet.Add((T)entity);
+          ProcessRelated(entity);
         }
-        Add((T)entity);
       }
-      ProcessRelated(entity);
-    
     }
+    private bool _inProcess = false;
 
     private void ProcessRelated(IEntity entity) {
-
+      
+      var aspect = entity.EntityAspect;
+      var parentAspect = ParentEntity.EntityAspect;
       var np = this.NavigationProperty;
       var invNp = np.Inverse;
-
-    //    var invNp = np.inverse;
-    //    var startIx = addsInProcess.length;
-    //    try {
-    //        adds.forEach(function (childEntity) {
-    //            addsInProcess.push(childEntity);
-    //            if (invNp) {
-    //                childEntity.setProperty(invNp.name, parentEntity);
-    //            } else {
-    //                // This occurs with a unidirectional 1-n navigation - in this case
-    //                // we need to update the fks instead of the navProp
-    //                var pks = parentEntity.entityType.keyProperties;
-    //                np.invForeignKeyNames.forEach(function (fk, i) {
-    //                    childEntity.setProperty(fk, parentEntity.getProperty(pks[i].name));
-    //                });
-    //            }
-    //        });
-    //    } finally {
-    //        addsInProcess.splice(startIx, adds.length);
-    //    }
-
+      if (invNp != null) {
+        aspect.SetValue(invNp, ParentEntity);
+      } else {
+        // This occurs with a unidirectional 1-n navigation - in this case
+        // we need to update the fks instead of the navProp
+        var pks = parentAspect.EntityType.KeyProperties;
+        np.InvForeignKeyNames.ForEach((fk, i) => {
+          entity.EntityAspect.SetValue(fk, parentAspect.GetValue(pks[i]));
+        });
+      }
     }
-    
+
+    #endregion
+
   }
 }
 
