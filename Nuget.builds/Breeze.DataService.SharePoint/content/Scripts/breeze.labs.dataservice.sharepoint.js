2d5b0b78914b6e04f92d0ca199678e6289eb7ad8
Ward Bell <wardbell@hotmail.com>
2014-02-13 21:37:06 -0800

    breeze.labs.dataservice.sharepoint.js v.0.1.3-pre and its nuget pkg

--- Nuget.builds/Breeze.DataService.SharePoint/content/Scripts/breeze.labs.dataservice.sharepoint.js
+++ Nuget.builds/Breeze.DataService.SharePoint/content/Scripts/breeze.labs.dataservice.sharepoint.js
@@ -1,7 +1,7 @@
 ﻿/*
  * Breeze Labs SharePoint 2013 OData DataServiceAdapter
  *
- *  v.0.1.2-pre
+ *  v.0.1.3-pre
  *
  * Registers a SharePoint 2013 OData DataServiceAdapter with Breeze
  *
@@ -23,13 +23,18 @@
  * 
  * This adapter also memoizes the type names it encounters 
  * by adding a 'typeMap' object to the JsonResultsAdapter.
+ *
+ * By default this adapter permits only one entity to be saved at a time.
+ * You can set 'adapter.saveOnlyOne' = false to allow more than one.
+ * Beware: when a multi-entity save fails, the cache is likely in an incorrect 
+ * and unstable state and the database data may be as well. You bear the risk.
  * 
  * Copyright 2014 IdeaBlade, Inc.  All Rights Reserved.
  * Licensed under the MIT License
  * http://opensource.org/licenses/mit-license.php
  * Authors: Ward Bell, Andrew Connell
  */
-(function (factory) {
+(function(factory) {
     if (breeze) {
         factory(breeze);
     } else if (typeof require === "function" && typeof exports === "object" && typeof module === "object") {
@@ -56,9 +61,10 @@
         getRequestDigest: undefined, // function that returns value for X-RequestDigest header
         initialize: initialize,
         jsonResultsAdapter: createJsonResultsAdapter(),
-        Q: Q, // assume Q.js in global namespace; you better set it if it's not
-        saveChanges: saveChanges
-};
+        Q: Q, // assume Q.js in global namespace; you better set it if it's not there
+        saveChanges: saveChanges,
+        saveOnlyOne: true, // false if you allow multiple entity saves; beware!
+    };
 
     breeze.config.registerAdapter("dataService", ctor);
 
@@ -68,17 +74,6 @@
         OData = core.requireLib("OData", "Needed to support remote OData services");
         OData.jsonHandler.recognizeDates = true;
     };
-
-    function defaultClientTypeNameToServer(clientTypeName) {
-        return 'SP.Data.' + clientTypeName + 'sListItem';
-    }
-
-    function defaultServerTypeNameToClient(serverTypeName) {
-        // strip off leading 'SP.Data.' and trailing 'sListItem'
-        var re = /^(SP\.Data.)(.*)(sListItem)$/;
-        var typeName = serverTypeName.replace(re, '$2');
-        return breeze.MetadataStore.normalizeTypeName(typeName);
-    }
 
     function createError(error, url) {
         // OData errors can have the message buried very deeply - and nonobviously
@@ -120,6 +115,7 @@
             var m = body.message || "";
             return ((typeof (m) === "string") ? m : m.value) + "; ";
         }
+
     }
 
     function createJsonResultsAdapter() {
@@ -151,11 +147,13 @@
 
             return result;
         };
-        
+
         // Determine if this is an Entity node and update the node appropriately if so
         function updateEntityNode(node, mappingContext, result) {
             var metadata = node.__metadata;
-            if (!metadata) { return; }
+            if (!metadata) {
+                return;
+            }
 
             var jrAdapter = mappingContext.jsonResultsAdapter;
             var typeMap = jrAdapter.typeMap;
@@ -170,7 +168,7 @@
             if (!entityType && rawTypeName) {
                 // Haven't see this rawTypeName before; add it to the typeMap
                 // Figure out what EntityType this is and remember it
-                
+
                 var typeName = jrAdapter.serverTypeNameToClient ?
                     jrAdapter.serverTypeNameToClient(rawTypeName) :
                     defaultServerTypeNameToClient(rawTypeName);
@@ -188,88 +186,110 @@
                 }
             }
         }
+
     }
 
-    function createSaveRequest(saveContext, entity) {
-        var request;
-        if (!entity) { return undefined; } // shouldn't be here
-
-        saveContext.originalEntity = entity;
+    function createSaveRequests(saveContext, saveBundle) {
 
         var entityManager = saveContext.entityManager;
         var helper = entityManager.helper;
+        var requestDigest = saveContext.requestDigest;
 
-        var aspect = entity.entityAspect;
-        var state = aspect.entityState;
-        var type = entity.entityType;
+        var originalEntities = saveContext.originalEntities = [];
+        var tempKeys = saveContext.tempKeys = [];
 
-        if (state.isAdded()) {
-            var rn = type.defaultResourceName;
-            if (!rn) {
-                throw new Error("Missing defaultResourceName for type "+type.name);
+        var requests = saveBundle.entities.map(createSaveRequest);
+        return requests;
+
+        function createSaveRequest(entity, index) {
+            var request;
+            originalEntities.push(entity);
+
+            var aspect = entity.entityAspect;
+            var state = aspect.entityState;
+            var type = entity.entityType;
+            var headers = {
+                'DataServiceVersion': "2.0", // Why?
+                'Accept': 'application/json;odata=verbose;',
+                'Content-Type': 'application/json;odata=verbose;',
+                'X-RequestDigest': saveContext.requestDigest
+            };
+
+            if (state.isAdded()) {
+                var rn = type.defaultResourceName;
+                if (!rn) {
+                    throw new Error("Missing defaultResourceName for type " + type.name);
+                }
+                request = {
+                    requestUri: entityManager.dataService.serviceName + rn,
+                    method: "POST",
+                    data: helper.unwrapInstance(entity, transformValue)
+                };
+                request.data['__metadata'] = {
+                    'type': saveContext.clientTypeNameToServer(type.shortName)
+                };
+                tempKeys[index] = aspect.getKey(); // DO NOT PUSH. Gaps expected!
+
+            } else if (state.isModified()) {
+                var data = helper.unwrapChangedValues(entity, entityManager.metadataStore, transformValue);
+                data.__metadata = { 'type': aspect.extraMetadata.type };
+                headers['X-HTTP-Method'] = 'MERGE';
+                request = {
+                    method: "POST",
+                    data: data
+                };
+                tweakUpdateDeleteMergeRequest();
+
+            } else if (state.isDeleted()) {
+                request = {
+                    method: "DELETE",
+                    data: null
+                };
+                tweakUpdateDeleteMergeRequest();
+            } else {
+                throw new Error("Cannot save an entity whose EntityState is " + state.name);
             }
-            request = {
-                requestUri: entityManager.dataService.serviceName + rn,
-                method: "POST",
-                headers: {},
-                data: helper.unwrapInstance(entity, transformValue)
-            };
-            request.data['__metadata'] = {
-                 'type': saveContext.clientTypeNameToServer(type.shortName)
-            };
-            saveContext.tempKey = aspect.getKey();
-        } else if (state.isModified()) {
-            var data = helper.unwrapChangedValues(entity, entityManager.metadataStore, transformValue);
-            data.__metadata = { 'type': aspect.extraMetadata.type };
-            request = {
-                method: "POST",
-                headers: { 'X-HTTP-Method': 'MERGE' },
-                data: data
-            };
-            tweakUpdateDeleteMergeRequest();
-            // should be a PATCH/MERGE
-        } else if (state.isDeleted()) {
-            request = {
-                method: "DELETE",
-                headers: {},
-                data: null
-            };
-            tweakUpdateDeleteMergeRequest();
-        } else {
-            return undefined; // Huh? Detached? How did it get here?
-        }
 
-        var headers = request.headers;
-        headers['DataServiceVersion'] = "2.0"; // Why?
-        headers['Accept'] = 'application/json;odata=verbose;';
-        headers['Content-Type'] = 'application/json;odata=verbose;';
+            request.headers = headers;
 
-        return request;
-
-        function tweakUpdateDeleteMergeRequest() {
-            var extraMetadata = aspect.extraMetadata;
-            if (!extraMetadata) {
-                throw new Error("Missing the OData metadata for an update/delete entity");
-            }
-            var uri = extraMetadata.uri || extraMetadata.id;
-            request.requestUri = uri;
-            if (extraMetadata.etag) {
-                request.headers["If-Match"] = extraMetadata.etag;
-            }
             return request;
-        }
 
-        function transformValue(prop, val) {
-            if (prop.isUnmapped) return undefined;
-            if (prop.dataType === breeze.DataType.DateTimeOffset) {
-                // The datajs lib tries to treat client dateTimes that are defined as DateTimeOffset on the server differently
-                // from other dateTimes. This fix compensates before the save.
-                val = val && new Date(val.getTime() - (val.getTimezoneOffset() * 60000));
-            } else if (prop.dataType.quoteJsonOData) {
-                val = val != null ? val.toString() : val;
+            function transformValue(prop, val) {
+                if (prop.isUnmapped) return undefined;
+                if (prop.dataType === breeze.DataType.DateTimeOffset) {
+                    // The datajs lib tries to treat client dateTimes that are defined as DateTimeOffset on the server differently
+                    // from other dateTimes. This fix compensates before the save.
+                    val = val && new Date(val.getTime() - (val.getTimezoneOffset() * 60000));
+                } else if (prop.dataType.quoteJsonOData) {
+                    val = val != null ? val.toString() : val;
+                }
+                return val;
             }
-            return val;
+
+            function tweakUpdateDeleteMergeRequest() {
+                var extraMetadata = aspect.extraMetadata;
+                if (!extraMetadata) {
+                    throw new Error("Missing the OData metadata for an update/delete entity");
+                }
+                var uri = extraMetadata.uri || extraMetadata.id;
+                request.requestUri = uri;
+                if (extraMetadata.etag) {
+                    headers["If-Match"] = extraMetadata.etag;
+                }
+                return request;
+            }
         }
+    }
+
+    function defaultClientTypeNameToServer(clientTypeName) {
+        return 'SP.Data.' + clientTypeName + 'sListItem';
+    }
+
+    function defaultServerTypeNameToClient(serverTypeName) {
+        // strip off leading 'SP.Data.' and trailing 'sListItem'
+        var re = /^(SP\.Data.)(.*)(sListItem)$/;
+        var typeName = serverTypeName.replace(re, '$2');
+        return breeze.MetadataStore.normalizeTypeName(typeName);
     }
 
     function executeQuery(mappingContext) {
@@ -308,78 +328,123 @@
     }
 
     function saveChanges(saveContext, saveBundle) {
-        var error;
-        if (saveBundle.entities.length > 1) {
-            error = new Error("The SharePoint OData data service can only save one entity at a time.");
-            return Q.reject(error);
+        var Q = this.Q;
+        if (this.saveOnlyOne && saveBundle.entities.length > 1) {
+            return Q.reject(new Error("Only one entity may be saved at a time."));
         }
-        var entity = saveBundle.entities[0];
-
         saveContext.clientTypeNameToServer = getClientTypeNameToServer(this);
+        saveContext.Q = Q;
+        saveContext.requestDigest = this.getRequestDigest ? this.getRequestDigest() : null;
 
-        var request = createSaveRequest(saveContext, entity);
-        if (!request) {
-            error = new Error("Could not compose valid save request.");
-            return Q.reject(error);
+        try {
+            var requests = createSaveRequests(saveContext, saveBundle);
+            var promises = sendSaveRequests(saveContext, requests); 
+            var comboPromise = Q.all(promises);
+            // $q.all waits for all to complete, Q.all quits on first failure
+            // It's bad regardless any of multiple save promises fail
+            return comboPromise
+                .then(saveSucceeded)
+                .then(null, saveFailed)
+
+        } catch (err) {
+            return Q.reject(err);
         }
 
-        if (this.getRequestDigest) {
-            request.headers['X-RequestDigest'] = this.getRequestDigest();
+        function saveSucceeded(promiseValues) {
+            return saveContext.saveResult; 
         }
 
-        var deferred = this.Q.defer();
-
-        OData.request({
-            requestUri: request.requestUri,
-            method: request.method,
-            headers: request.headers,
-            data: request.data 
-        }, saveSucceeded, saveFailed);
-
-        return deferred.promise;
-
-        function saveSucceeded(data, response) {
-            var statusCode = response.statusCode;
-            if ((!statusCode) || statusCode >= 400) {
-                return deferred.reject(createError(response, url));
+        function saveFailed(error) {
+            var requestCount = comboPromise.length;
+            if (requestCount < 2) {
+                // Only one request so failure is probably no big deal
+                return Q.reject(error);
             }
+            // Multiple requests => serious trouble.
+            var saveResult = saveContext.saveResult;
+            var savedCount = saveResult.entities.length;
+            var emsg = "At least one of the " + requestCount + " save requests failed " +
+                "with the message '"+ (error.message ? error.message : error) +"'.";
+            emsg += (savedCount ? "At least "+ savedCount : "No");
+            emsg += " requests are known to have succeeded on the server, perhaps more. " +
+            "Regardless, the EntityManager cache is in an unstable state. ;"
 
-            var entities = [];
-            var keyMappings = [];
-            var saveResult = { entities: entities, keyMappings: keyMappings };
-            var rawEntity = response.data;
-            if (rawEntity) {
-                var tempKey = saveContext.tempKey;
-                if (tempKey) {
-                    var entityType = tempKey.entityType;
-                    if (entityType.autoGeneratedKeyType !== breeze.AutoGeneratedKeyType.None) {
-                        var tempValue = tempKey.values[0];
-                        var realKey = entityType.getEntityKeyFromRawEntity(
-                                rawEntity, breeze.DataProperty.getRawValueFromServer);
-                        var keyMapping = {
-                            entityTypeName: entityType.name,
-                            tempValue: tempValue,
-                            realValue: realKey.values[0]
-                        };
-                        keyMappings.push(keyMapping);
-                    }
-                }
-                entities.push(rawEntity);
-            } else {
-                var saved = saveContext.originalEntity;
-                var etag = response.headers['ETag'];
-                if (etag) {
-                    saved.entityAspect.extraMetadata.etag = etag;
-                }
-                entities.push(saved);
-            }
-
-            return deferred.resolve(saveResult);
-        }
-
-        function saveFailed(err, url) {
-            return deferred.reject(createError(err, url));
+            var newErr = new Error(emsg);
+            newErr.originalError = error;
+            // Attaching the 'saveContext' for diagnostic purposes but
+            // it is unlikely that there is any truly safe recovery and
+            // we recommend, at a minimum, that you call manager.rejectChanges()
+            // and reload pertinent entity data from the server.
+            newErr.failedSaveContext = saveContext;
+            return Q.reject(newErr);
         }
     };
 
+    function sendSaveRequests(saveContext, requests) {
+        // Sends each prepared save request and processes the promised results
+        // returns a single "comboPromise" that waits for the individual promises to complete
+        // Todo: What happens when there are a gazillion async requests?
+        var Q = saveContext.Q;
+        var savedEntities = [];
+        var keyMappings = [];
+        var saveResult = { entities: savedEntities, keyMappings: keyMappings };
+        saveContext.saveResult = saveResult;
+
+        return requests.map(sendSaveRequest);
+
+        function sendSaveRequest(request, index) {
+            var deferred = Q.defer();
+
+            OData.request({
+                    requestUri: request.requestUri,
+                    method: request.method,
+                    headers: request.headers,
+                    data: request.data
+                },
+                requestSucceeded,
+                requestFailed);
+
+            return deferred.promise;
+
+            function requestSucceeded(data, response) {
+                var statusCode = response.statusCode;
+                if ((!statusCode) || statusCode >= 400) {
+                    return deferred.reject(createError(response, url));
+                }
+
+                var rawEntity = response.data;
+                if (rawEntity) {
+                    var tempKey = saveContext.tempKeys[index];
+                    if (tempKey) {
+                        var entityType = tempKey.entityType;
+                        if (entityType.autoGeneratedKeyType !== breeze.AutoGeneratedKeyType.None) {
+                            var tempValue = tempKey.values[0];
+                            var realKey = entityType.getEntityKeyFromRawEntity(
+                                rawEntity, breeze.DataProperty.getRawValueFromServer);
+                            var keyMapping = {
+                                entityTypeName: entityType.name,
+                                tempValue: tempValue,
+                                realValue: realKey.values[0]
+                            };
+                            keyMappings.push(keyMapping);
+                        }
+                    }
+                    savedEntities.push(rawEntity);
+                } else {
+                    var saved = saveContext.originalEntities[index];
+                    var etag = response.headers['ETag'];
+                    if (etag) {
+                        saved.entityAspect.extraMetadata.etag = etag;
+                    }
+                    savedEntities.push(saved);
+                }
+
+                return deferred.resolve(true);
+            }
+
+            function requestFailed(err, url) {
+                return deferred.reject(createError(err, url));
+            }
+        }
+    }
 });
