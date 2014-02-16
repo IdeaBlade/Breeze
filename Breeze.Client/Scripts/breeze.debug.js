4416c1db17fa5d914b3bff1aca411172184d98aa
Ward Bell <wardbell@hotmail.com>
2014-02-15 21:27:23 -0800

    Settings object of b00_breeze.ajax.angular and b00_breeze.ajax.jQuery accepts a "headers" property Needed for SharePoint adapter. See OnTIme F#2247 "Extend ajax adapter config to accept headers from dataservice adapters" breeze.debug included for testing purposes but was not really rebuilt.

--- Breeze.Client/Scripts/breeze.debug.js
+++ Breeze.Client/Scripts/breeze.debug.js
@@ -14803,7 +14803,8 @@ breeze.SaveOptions= SaveOptions;
             url: config.url,
             dataType: config.dataType,
             contentType: config.contentType,
-            crossDomain: config.crossDomain
+            crossDomain: config.crossDomain,
+            headers: config.headers || {}
         }
 
         if (config.params) {
@@ -14821,6 +14822,8 @@ breeze.SaveOptions= SaveOptions;
         if (!core.isEmpty(this.defaultSettings)) {
             var compositeConfig = core.extend({}, this.defaultSettings);
             ngConfig = core.extend(compositeConfig, ngConfig);
+            // extend is shallow; extend headers separately
+            ngConfig.headers = core.extend(this.defaultSettings.headers, ngConfig.headers);
         }
 
         httpService(ngConfig).success(function (data, status, headers, xconfig) {
@@ -14918,12 +14921,15 @@ breeze.SaveOptions= SaveOptions;
             data: config.params || config.data,
             dataType: config.dataType,
             contentType: config.contentType,
-            crossDomain: config.crossDomain
+            crossDomain: config.crossDomain,
+            headers: config.headers || {}
         }
         
         if (!core.isEmpty(this.defaultSettings)) {
             var compositeConfig = core.extend({}, this.defaultSettings);
             jqConfig = core.extend(compositeConfig, jqConfig);
+            // extend is shallow; extend headers separately
+            jqConfig.headers = core.extend(this.defaultSettings.headers, jqConfig.headers);
         }
         
         jqConfig.success = function (data, textStatus, XHR) {
