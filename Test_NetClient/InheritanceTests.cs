5d8f59022ac5523ed51f5b09b5be27a50066bb0d
Jay Traband <jayt@ideablade.com>
2014-03-14 12:46:46 -0700

    breeze.net - starting inheritance

---
+++ Test_NetClient/InheritanceTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using System.Linq;
+using Breeze.Core;
+using Breeze.NetClient;
+using System.Collections.Generic;
+using Foo;
+
+namespace Test_NetClient {
+
+  [TestClass]
+  public class InheritanceTests {
+
+    private Task<EntityManager> _emTask = null;
+    private EntityManager _em1;
+    
+
+    [TestInitialize]
+    public void TestInitializeMethod() {
+      _emTask = SetUpAsync();
+    }
+
+    public async Task<EntityManager> SetUpAsync() {
+      var serviceName = "http://localhost:7150/breeze/Inheritance/";
+      
+      if (MetadataStore.Instance.EntityTypes.Count == 0) {
+        _em1 = new EntityManager(serviceName);
+        await _em1.FetchMetadata();
+      } else {
+        _em1 = new EntityManager(serviceName);
+      }
+      return _em1;
+      
+    }
+
+    [TestCleanup]
+    public void TearDown() {
+      
+    }
+
+    [TestMethod]
+    public async Task SimpleQuery() {
+      await _emTask;
+      var q = new EntityQuery<Customer>();
+
+      
+    }
+
+    
+    
+  }
+}
+
+  
