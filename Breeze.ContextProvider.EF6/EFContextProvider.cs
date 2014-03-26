d30a0e2daef87d9ecae861f5a96275c19642b3e7
Andrew Veresov <andrew.veresov@gmail.com>
2014-03-26 13:26:18 +0400

    Removed unused namespace

--- Breeze.ContextProvider.EF6/EFContextProvider.cs
+++ Breeze.ContextProvider.EF6/EFContextProvider.cs
@@ -20,7 +20,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.Remoting.Messaging;
 using System.Xml;
 using System.Xml.Linq;
 using Newtonsoft.Json;
