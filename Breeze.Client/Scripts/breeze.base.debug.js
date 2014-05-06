bc21f9ee51942984344cc40491a56ff057991740
Patrick Walters <patrickwaltersc21@yahoo.com>
2014-05-06 09:39:35 -0500

    copy was being create as a global

--- Breeze.Client/Scripts/breeze.base.debug.js
+++ Breeze.Client/Scripts/breeze.base.debug.js
@@ -9966,7 +9966,7 @@ var EntityQuery = (function () {
             if (that[propName] === value) return that;
         }
         // copying QueryOptions is safe because they are are immutable; 
-        copy = __extend(new EntityQuery(), that, [
+        var copy = __extend(new EntityQuery(), that, [
             "resourceName",
             "entityType",
             "wherePredicate",
